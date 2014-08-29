b3272f195f9c6bc60325ce6fa507d1ce09d5a7c6
Craig Bryant <craig.bryant@hp.com>
2014-08-29 14:16:10 -0600

    Change to use monasca everywhere
    Change packages from com.hpcloud.mon to monasca.api
    
    Remove places where hpcloudsvc was used
    
    Rename service from mon-api to monasca-api. Move config file
    to /etc/monasca, jar to /opt/monasca
    
    Renamed files from mon- to monasca-
    
    Use the monasca group.  Required so API can write to
    /var/log/monasca and read config file in /etc/monasca
    
    Change user from mon_api to monasca_api
    
    Change-Id: Idaefe138629595507f3d38a533381408cc6f32e3

---
+++ src/test/java/monasca/api/infrastructure/persistence/influxdb/UtilsTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package monasca.api.infrastructure.persistence.influxdb;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.testng.annotations.Test;
+
+@Test
+public class UtilsTest {
+  public void SQLSanitizerSanitizeGoodDataTest() throws Exception {
+    String goodString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" + "-_" +
+        ".?/%=&鱼鸟";
+
+    assert (goodString.equals(Utils.SQLSanitizer.sanitize(goodString)));
+  }
+
+  @Test(expectedExceptions = {Exception.class})
+  public void SQLSanitizerSanitizeBadDataTest1() throws Exception {
+    String badStringWithSemicolon = "abcdefghijklmnopqrs;tuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" + "-_.";
+
+    assert (badStringWithSemicolon.equals(Utils.SQLSanitizer.sanitize(badStringWithSemicolon)));
+  }
+
+  @Test(expectedExceptions = {Exception.class})
+  public void SQLSanitizerSanitizeBadDataTest2() throws Exception {
+    String badStringWithSingleQuote = "'a'bcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" + "-_.";
+
+    assert (badStringWithSingleQuote.equals(Utils.SQLSanitizer.sanitize(badStringWithSingleQuote)));
+  }
+
+  public void whereClauseBuilderBuildTimePartTest() {
+    String expectedResult = " and time > 1388538061s and time < 1388538062s";
+    DateTime startTime = new DateTime(2014, 01, 01, 01, 01, 01, DateTimeZone.UTC);
+    DateTime endTime = new DateTime(2014, 01, 01, 01, 01, 02, DateTimeZone.UTC);
+
+    assert (expectedResult.equals(Utils.WhereClauseBuilder.buildTimePart(startTime, endTime)));
+  }
+
+  public void whereClauseBuilderBuildDimsPartTest1() throws Exception {
+    String expectedResult = "";
+    Map<String, String> dimsMap = new HashMap<>();
+    assert (expectedResult.equals(Utils.WhereClauseBuilder.buildDimsPart(dimsMap)));
+  }
+
+  public void whereClauseBuilderBuildDimsPartTest2() throws Exception {
+    String expectedResult = " and foo = 'bar'";
+    Map<String, String> dimsMap = new HashMap<>();
+    dimsMap.put("foo", "bar");
+    assert (expectedResult.equals(Utils.WhereClauseBuilder.buildDimsPart(dimsMap)));
+  }
+
+  public void whereClauseBuilderBuildDimsPartTest3() throws Exception {
+    String expectedResult = " and foo = 'bar' and biz = 'baz'";
+    Map<String, String> dimsMap = new LinkedHashMap<>();
+    dimsMap.put("foo", "bar");
+    dimsMap.put("biz", "baz");
+    assert (expectedResult.equals(Utils.WhereClauseBuilder.buildDimsPart(dimsMap)));
+  }
+}
---
+++ src/test/java/monasca/api/resource/AlarmResourceTest.java
@@ -0,0 +1,380 @@
+/*
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package monasca.api.resource;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyMap;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.core.MediaType;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.testng.annotations.Test;
+
+import monasca.api.app.AlarmService;
+import monasca.api.app.command.CreateAlarmCommand;
+import monasca.api.app.command.UpdateAlarmCommand;
+import com.hpcloud.mon.common.model.alarm.AlarmExpression;
+import com.hpcloud.mon.common.model.alarm.AlarmState;
+import monasca.api.domain.exception.EntityNotFoundException;
+import monasca.api.domain.model.alarm.Alarm;
+import monasca.api.domain.model.alarm.AlarmRepository;
+import monasca.api.domain.model.alarmstatehistory.AlarmStateHistory;
+import monasca.api.domain.model.alarmstatehistory.AlarmStateHistoryRepository;
+import monasca.api.domain.model.common.Link;
+import monasca.api.resource.exception.ErrorMessages;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.GenericType;
+
+@Test
+public class AlarmResourceTest extends AbstractMonApiResourceTest {
+  private String expression;
+  private Alarm alarm;
+  private Alarm alarmItem;
+  private AlarmService service;
+  private AlarmRepository repo;
+  private AlarmStateHistoryRepository stateHistoryRepo;
+  private List<String> alarmActions;
+
+  @Override
+  @SuppressWarnings("unchecked")
+  protected void setupResources() throws Exception {
+    super.setupResources();
+
+    expression = "avg(disk_read_ops{service=hpcs.compute, instance_id=937}) >= 90";
+    alarmItem =
+        new Alarm("123", "Disk Exceeds 1k Operations", null, "LOW", expression, AlarmState.OK,
+            true, null, null, null);
+    alarmActions = new ArrayList<String>();
+    alarmActions.add("29387234");
+    alarmActions.add("77778687");
+    alarm =
+        new Alarm("123", "Disk Exceeds 1k Operations", null, "LOW", expression, AlarmState.OK,
+            true, alarmActions, null, null);
+
+    service = mock(AlarmService.class);
+    when(
+        service.create(eq("abc"), eq("Disk Exceeds 1k Operations"), any(String.class), eq("LOW"),
+            eq(expression), eq(AlarmExpression.of(expression)), any(List.class), any(List.class),
+            any(List.class))).thenReturn(alarm);
+
+    repo = mock(AlarmRepository.class);
+    when(repo.findById(eq("abc"), eq("123"))).thenReturn(alarm);
+    when(repo.find(anyString(), anyString(), (Map<String, String>) anyMap(), any(String.class)))
+        .thenReturn(Arrays.asList(alarmItem));
+
+    stateHistoryRepo = mock(AlarmStateHistoryRepository.class);
+
+    addResources(new AlarmResource(service, repo, stateHistoryRepo));
+  }
+
+  @SuppressWarnings("unchecked")
+  public void shouldCreate() {
+    ClientResponse response =
+        createResponseFor(new CreateAlarmCommand("Disk Exceeds 1k Operations", null, expression,
+            "LOW", alarmActions, null, null));
+
+    assertEquals(response.getStatus(), 201);
+    Alarm newAlarm = response.getEntity(Alarm.class);
+    String location = response.getHeaders().get("Location").get(0);
+    assertEquals(location, "/v2.0/alarms/" + newAlarm.getId());
+    assertEquals(newAlarm, alarm);
+    verify(service).create(eq("abc"), eq("Disk Exceeds 1k Operations"), any(String.class),
+        eq("LOW"), eq(expression), eq(AlarmExpression.of(expression)), any(List.class),
+        any(List.class), any(List.class));
+  }
+
+  public void shouldUpdate() {
+    when(
+        service.update(eq("abc"), eq("123"), any(AlarmExpression.class),
+            any(UpdateAlarmCommand.class))).thenReturn(alarm);
+    ClientResponse response =
+        client()
+            .resource("/v2.0/alarms/123")
+            .header("X-Tenant-Id", "abc")
+            .header("Content-Type", MediaType.APPLICATION_JSON)
+            .put(
+                ClientResponse.class,
+                new UpdateAlarmCommand("Disk Exceeds 1k Operations", null, expression, "LOW",
+                    AlarmState.ALARM, true, alarmActions, null, null));
+
+    assertEquals(response.getStatus(), 200);
+    verify(service).update(eq("abc"), eq("123"), any(AlarmExpression.class),
+        any(UpdateAlarmCommand.class));
+  }
+
+  public void shouldErrorOnCreateWithInvalidMetricName() {
+    String expression = "avg(foo{service=hpcs.compute, instance_id=937}) >= 90";
+    ClientResponse response =
+        createResponseFor(new CreateAlarmCommand("Disk Exceeds 1k Operations", null, expression,
+            "LOW", alarmActions, null, null));
+
+    ErrorMessages.assertThat(response.getEntity(String.class)).matches("unprocessable_entity", 422,
+        "foo is not a valid metric name for namespace hpcs.compute");
+  }
+
+  public void shouldErrorOnCreateWithInvalidDimensions() {
+    String expression = "avg(disk_read_ops{service=hpcs.compute, instance_id=937, foo=bar}) >= 90";
+    ClientResponse response =
+        createResponseFor(new CreateAlarmCommand("Disk Exceeds 1k Operations", null, expression,
+            "LOW", alarmActions, null, null));
+
+    ErrorMessages.assertThat(response.getEntity(String.class)).matches("unprocessable_entity", 422,
+        "foo is not a valid dimension name for service hpcs.compute");
+  }
+
+  public void shouldErrorOnCreateWithDuplicateDimensions() {
+    String expression =
+        "avg(hpcs.compute{instance_id=937, instance_id=123, az=2, instance_uuid=abc123, metric_name=disk_read_ops}) >= 90";
+    ClientResponse response =
+        createResponseFor(new CreateAlarmCommand("Disk Exceeds 1k Operations", null, expression,
+            "LOW", alarmActions, null, null));
+
+    ErrorMessages.assertThat(response.getEntity(String.class)).matches("unprocessable_entity", 422,
+        "The alarm expression is invalid",
+        "More than one value was given for dimension instance_id");
+  }
+
+  @SuppressWarnings("unchecked")
+  public void shouldNotRequireDimensionsForCustomNamespace() {
+    String expression = "avg(foo{metric_name=bar}) >= 90";
+    when(
+        service.create(eq("abc"), eq("Disk Exceeds 1k Operations"), any(String.class), eq("LOW"),
+            eq(expression), eq(AlarmExpression.of(expression)), any(List.class), any(List.class),
+            any(List.class))).thenReturn(alarm);
+    ClientResponse response =
+        createResponseFor(new CreateAlarmCommand("Disk Exceeds 1k Operations", null, expression,
+            "LOW", alarmActions, null, null));
+    assertEquals(response.getStatus(), 201);
+  }
+
+  public void shouldErrorOnCreateWithInvalidJson() {
+    ClientResponse response =
+        createResponseFor("{\"alarmasdf\"::{\"name\":\"Disk Exceeds 1k Operations\"}}");
+
+    ErrorMessages.assertThat(response.getEntity(String.class)).matches("bad_request", 400,
+        "Unable to process the provided JSON", "Unexpected character (':'");
+  }
+
+  public void shouldErrorOnCreateWithInvalidOperator() {
+    String expression =
+        "avg(hpcs.compute{instance_id=937, az=2, instance_uuid=0ff588fc-d298-482f-bb11-4b52d56801a4, metric_name=disk_read_ops}) & 90";
+    ClientResponse response =
+        createResponseFor(new CreateAlarmCommand("Disk Exceeds 1k Operations", null, expression,
+            "LOW", alarmActions, null, null));
+
+    ErrorMessages.assertThat(response.getEntity(String.class)).matches("unprocessable_entity", 422,
+        "The alarm expression is invalid", "Syntax Error");
+  }
+
+  public void shouldErrorOnCreateWith0Period() {
+    String expression =
+        "avg(hpcs.compute{instance_id=937, az=2, instance_uuid=0ff588fc-d298-482f-bb11-4b52d56801a4, metric_name=disk_read_ops},0) >= 90";
+    ClientResponse response =
+        createResponseFor(new CreateAlarmCommand("Disk Exceeds 1k Operations", null, expression,
+            "LOW", alarmActions, null, null));
+
+    ErrorMessages.assertThat(response.getEntity(String.class)).matches("unprocessable_entity", 422,
+        "Period must not be 0");
+  }
+
+  public void shouldErrorOnCreateWithNonMod60Period() {
+    String expression =
+        "avg(hpcs.compute{instance_id=937, az=2, instance_uuid=0ff588fc-d298-482f-bb11-4b52d56801a4, metric_name=disk_read_ops},61) >= 90";
+    ClientResponse response =
+        createResponseFor(new CreateAlarmCommand("Disk Exceeds 1k Operations", null, expression,
+            "LOW", alarmActions, null, null));
+
+    ErrorMessages.assertThat(response.getEntity(String.class)).matches("unprocessable_entity", 422,
+        "Period 61 must be a multiple of 60");
+  }
+
+  public void shouldErrorOnCreateWithPeriodsLessThan1() {
+    String expression =
+        "avg(hpcs.compute{instance_id=937, az=2, instance_uuid=0ff588fc-d298-482f-bb11-4b52d56801a4, metric_name=disk_read_ops}) >= 90 times 0";
+    ClientResponse response =
+        createResponseFor(new CreateAlarmCommand("Disk Exceeds 1k Operations", null, expression,
+            "LOW", alarmActions, null, null));
+
+    ErrorMessages.assertThat(response.getEntity(String.class)).matches("unprocessable_entity", 422,
+        "Periods 0 must be greater than or equal to 1");
+  }
+
+  public void shouldErrorOnCreateWithPeriodTimesPeriodsGT2Weeks() {
+    String expression =
+        "avg(hpcs.compute{instance_id=937, az=2, instance_uuid=0ff588fc-d298-482f-bb11-4b52d56801a4, metric_name=disk_read_ops},60) >= 90 times 20161";
+    ClientResponse response =
+        createResponseFor(new CreateAlarmCommand("Disk Exceeds 1k Operations", null, expression,
+            "LOW", alarmActions, null, null));
+
+    ErrorMessages.assertThat(response.getEntity(String.class)).matches("unprocessable_entity", 422,
+        "Period 60 times 20161 must total less than 2 weeks in seconds (1209600)");
+  }
+
+  public void shouldErrorOnCreateWithTooLongName() {
+    String expression =
+        "avg(hpcs.compute{instance_id=937, az=2, instance_uuid=0ff588fc-d298-482f-bb11-4b52d56801a4, metric_name=disk_read_ops}) >= 90";
+    ClientResponse response =
+        createResponseFor(new CreateAlarmCommand(
+            "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+                + "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+                + "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789",
+            null, "LOW", expression, alarmActions, null, null));
+
+    ErrorMessages
+        .assertThat(response.getEntity(String.class))
+        .matches(
+            "unprocessable_entity",
+            422,
+            "Name 012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789 must be 255 characters or less");
+  }
+
+  public void shouldErrorOnCreateWithTooLongAlarmAction() {
+    alarmActions = new ArrayList<String>();
+    alarmActions.add("012345678901234567890123456789012345678901234567890");
+    ClientResponse response =
+        createResponseFor(new CreateAlarmCommand("Disk Exceeds 1k Operations", null, "LOW",
+            expression, alarmActions, null, null));
+
+    ErrorMessages
+        .assertThat(response.getEntity(String.class))
+        .matches("unprocessable_entity", 422,
+            "Alarm action 012345678901234567890123456789012345678901234567890 must be 50 characters or less");
+  }
+
+  @SuppressWarnings("unchecked")
+  public void shouldList() {
+    List<Alarm> alarms =
+        client().resource("/v2.0/alarms").header("X-Tenant-Id", "abc")
+            .get(new GenericType<List<Alarm>>() {});
+
+    assertEquals(alarms, Arrays.asList(alarmItem));
+    verify(repo).find(eq("abc"), anyString(), (Map<String, String>) anyMap(), any(String.class));
+  }
+
+  @SuppressWarnings("unchecked")
+  public void shouldListByName() throws Exception {
+    List<Alarm> alarms =
+        client().resource("/v2.0/alarms?name=" + URLEncoder.encode("foo bar baz", "UTF-8"))
+            .header("X-Tenant-Id", "abc").get(new GenericType<List<Alarm>>() {});
+
+    assertEquals(alarms, Arrays.asList(alarmItem));
+    verify(repo).find(eq("abc"), eq("foo bar baz"), (Map<String, String>) anyMap(),
+        any(String.class));
+  }
+
+  public void shouldGet() {
+    assertEquals(client().resource("/v2.0/alarms/123").header("X-Tenant-Id", "abc")
+        .get(Alarm.class), alarm);
+    verify(repo).findById(eq("abc"), eq("123"));
+  }
+
+  public void should404OnGetInvalid() {
+    doThrow(new EntityNotFoundException(null)).when(repo).findById(eq("abc"), eq("999"));
+
+    try {
+      client().resource("/v2.0/alarms/999").header("X-Tenant-Id", "abc").get(Alarm.class);
+      fail();
+    } catch (Exception e) {
+      assertTrue(e.getMessage().contains("404"));
+    }
+  }
+
+  public void shouldDelete() {
+    ClientResponse response =
+        client().resource("/v2.0/alarms/123").header("X-Tenant-Id", "abc")
+            .delete(ClientResponse.class);
+    assertEquals(response.getStatus(), 204);
+    verify(service).delete(eq("abc"), eq("123"));
+  }
+
+  public void should404OnDeleteInvalid() {
+    doThrow(new EntityNotFoundException(null)).when(service).delete(eq("abc"), eq("999"));
+
+    try {
+      client().resource("/v2.0/alarms/999").header("X-Tenant-Id", "abc").delete();
+      fail();
+    } catch (Exception e) {
+      assertTrue(e.getMessage().contains("404"));
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  public void should500OnInternalException() {
+    doThrow(new RuntimeException("")).when(repo).find(anyString(), anyString(),
+        (Map<String, String>) anyObject(), (String) anyObject());
+
+    try {
+      client().resource("/v2.0/alarms").header("X-Tenant-Id", "abc").get(List.class);
+      fail();
+    } catch (Exception e) {
+      assertTrue(e.getMessage().contains("500"));
+    }
+  }
+
+  public void shouldHydateLinksOnList() {
+    List<Link> expected =
+        Arrays.asList(new Link("self", "/v2.0/alarms/123"), new Link("history",
+            "/v2.0/alarms/123/history"));
+    List<Link> links =
+        client().resource("/v2.0/alarms").header("X-Tenant-Id", "abc")
+            .get(new GenericType<List<Alarm>>() {}).get(0).getLinks();
+    assertEquals(links, expected);
+  }
+
+  public void shouldHydateLinksOnGet() {
+    List<Link> links =
+        Arrays.asList(new Link("self", "/v2.0/alarms/123"), new Link("history",
+            "/v2.0/alarms/123/history"));
+    assertEquals(client().resource("/v2.0/alarms/123").header("X-Tenant-Id", "abc")
+        .get(Alarm.class).getLinks(), links);
+  }
+
+  public void shouldGetAlarmStateHistory() throws Exception {
+    AlarmStateHistory history1 =
+        new AlarmStateHistory("123", AlarmState.OK, AlarmState.ALARM, "foo", "foobar",
+            new DateTime(2014, 1, 1, 1, 1, DateTimeZone.UTC));
+    AlarmStateHistory history2 =
+        new AlarmStateHistory("123", AlarmState.ALARM, AlarmState.OK, "foo", "foobar",
+            new DateTime(2014, 1, 1, 1, 1, DateTimeZone.UTC));
+    List<AlarmStateHistory> expected = Arrays.asList(history1, history2);
+
+    when(stateHistoryRepo.findById(eq("abc"), eq("123"))).thenReturn(expected);
+
+    assertEquals(client().resource("/v2.0/alarms/123/state-history").header("X-Tenant-Id", "abc")
+        .get(new GenericType<List<AlarmStateHistory>>() {}), expected);
+  }
+
+  private ClientResponse createResponseFor(Object request) {
+    return client().resource("/v2.0/alarms").header("X-Tenant-Id", "abc")
+        .header("Content-Type", MediaType.APPLICATION_JSON).post(ClientResponse.class, request);
+  }
+}
