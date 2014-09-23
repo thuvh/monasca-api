fb1aecd6a785dca9fc0efb045a22dce57a02a96e
Roland Hochmuth <roland.hochmuth@hp.com>
2014-09-22 19:53:27 -0600

    Start of initial reference implementation and re-factored
    Change-Id: I90d09331a41f15c47870cd7690b22405087ff7a4

---
+++ monasca/common/messaging/publisher_factory.py
@@ -0,0 +1,27 @@
+# Copyright 2014 Hewlett-Packard
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from oslo.config import cfg
+from monasca.common.messaging import publisher
+from monasca.common.messaging import kafka_publisher
+from monasca.common.messaging import rabbitmq_publisher
+
+def create_publisher():
+    message_queue = cfg.CONF.messaging.message_queue
+    if message_queue == 'kafka':
+        return kafka_publisher.KafkaPublisher()
+    elif message_queue == 'rabbitmq':
+        return rabbitmq_publisher.RabbitmqPublisher()
+    else:
+        return publisher.Publisher()
