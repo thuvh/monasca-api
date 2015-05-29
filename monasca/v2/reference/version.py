767b839b4eba65c6ec936a9bd8f7699300cb4b99
SamKirsch10 <sam.kirsch@hp.com>
2015-05-29 13:46:26 -0600

    added in version return for python api
    Change-Id: Id335c28068721fd057d30be54c6611edcd9d7d61

---
+++ monasca/v2/reference/version.py
@@ -0,0 +1,50 @@
+# Copyright 2015 Hewlett-Packard
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import falcon
+
+from monasca.common import resource_api
+from monasca.openstack.common import log
+from monasca.v2.reference import helpers
+
+LOG = log.getLogger(__name__)
+CURRENTVERSION = 'v2.0'
+
+
+class Version(object):
+
+    @resource_api.Restify('/', method='get')
+    def do_get_version(self, req, res):
+        result = {u'links': [{
+            u'rel': u'self', u'href': req.uri.decode('utf8')}],
+            u'elements': [{
+                u'id': CURRENTVERSION,
+                u'links': [{
+                    u'rel': u'self',
+                    u'href': req.uri.decode('utf8') + CURRENTVERSION
+                }],
+                u'status': 'CURRENT', u'updated': '2015-05-19T19:21:00Z'}
+            ]}
+        res.body = helpers.dumpit_utf8(result)
+        res.status = falcon.HTTP_200
+
+    @resource_api.Restify('/{version_id}')
+    def do_get_specific_version(self, req, res, version_id):
+        result = {u'id': version_id,
+                  u'links': [{
+                      u'rel': u'self',
+                      u'href': req.uri.decode('utf8')
+                  }],
+                  u'status': 'CURRENT', u'updated': '2015-05-19T19:21:00Z'
+                  }
