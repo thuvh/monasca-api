dcf02d9c379dc6f7db6e55fd0ebfc766ae732d6d
SamKirsch10 <sam.kirsch@hp.com>
2015-06-11 13:12:49 -0600

    added version for restapi and fixed requirements to = 0.2
    Change-Id: I499da5caea26ac104ceda896cb795ee9a595def9

--- monasca_api/v2/reference/versions.py
+++ monasca_api/v2/reference/versions.py
@@ -11,12 +11,25 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+import json
 
+import falcon
 
 from monasca_api.api import versions_api
 from monasca_api.openstack.common import log
 
 LOG = log.getLogger(__name__)
+VERSIONS = {
+    u'v2.0': {
+        u'id': 'v2.0',
+        u'links': {
+            u'rel': 'self',
+            u'href': 'v2.0'
+        },
+        u'status': 'CURRENT',
+        u'updated': "2013-03-06T00:00:00Z"
+    }
+}
 
 
 class Versions(versions_api.VersionsAPI):
@@ -24,5 +37,33 @@ class Versions(versions_api.VersionsAPI):
     def __init__(self):
         super(Versions, self).__init__()
 
-    def on_get(self, req, res, id):
-        res.status = '501 Not Implemented'
+    def on_get(self, req, res, version_id=None):
+        result = {
+            u'links': [],
+            u'elements': []
+        }
+        links = {
+            u'rel': 'self',
+            u'href': req.uri.decode('utf8')
+        }
+        result[u'links'].append(links)
+        if version_id is None:
+            for version in VERSIONS:
+                VERSIONS[version][u'links'][u'href'] = (
+                    req.uri.decode('utf8') +
+                    VERSIONS[version][u'links'][u'href']
+                )
+                result[u'elements'].append(VERSIONS[version])
+            res.body = json.dumps(result)
+            res.status = falcon.HTTP_200
+        else:
+            if version_id in VERSIONS:
+                VERSIONS[version_id][u'links'][u'href'] = (
+                    req.uri.decode('utf8')
+                )
+                result[u'elements'].append(VERSIONS[version_id])
+                res.body = json.dumps(result)
+                res.status = falcon.HTTP_200
+            else:
+                res.body = 'Invalid Version ID'
+                res.status = falcon.HTTP_400
