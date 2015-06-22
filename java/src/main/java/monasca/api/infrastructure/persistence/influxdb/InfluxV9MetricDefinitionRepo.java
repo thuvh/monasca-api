a8b39e745d40944c3b822b864c68aae9e5c5f9ca
Deklan Dieterly <deklan.dieterly@hp.com>
2015-06-22 15:23:32 -0600

    Make changes for Influxdb 0.9 Latest Stable
    Do not return _key as dimension when doing metric list.
    
    Change-Id: I2bf743977c6bcbdad65f33e05fce7422a722f8e0

--- java/src/main/java/monasca/api/infrastructure/persistence/influxdb/InfluxV9MetricDefinitionRepo.java
+++ java/src/main/java/monasca/api/infrastructure/persistence/influxdb/InfluxV9MetricDefinitionRepo.java
@@ -182,10 +182,12 @@ public class InfluxV9MetricDefinitionRepo implements MetricDefinitionRepo {
     Map<String, String> dims = new HashMap<>();
 
     for (int i = 0; i < cols.length; ++i) {
-      if (!cols[i].equals("_region")
-          && !cols[i].equals("_tenant_id")
-          && !cols[i].equals("_id")) {
+
+      // Dimension names that start with underscore are reserved. (_key, _id, _region, _tenant_id)
+      if (!cols[i].startsWith("_")) {
+
         if (!vals[i].equalsIgnoreCase("null")) {
+
           dims.put(cols[i], vals[i]);
         }
       }
