2b35445c4b319c62517040d75cfae7943f726cb1
Deklan Dieterly <deklan.dieterly@hpe.com>
2015-09-25 08:39:22 -0600

    Install schema
    Change-Id: Ie3b263988256f7bcbda0616d1c8264b49c9e88b5

---
+++ devstack/files/env.sh
@@ -0,0 +1,7 @@
+# Environment variables for use with python-monascaclient running via monaca-vagrant
+
+. /opt/monasca/bin/activate
+export OS_USERNAME=mini-mon
+export OS_PASSWORD=password
+export OS_PROJECT_NAME=mini-mon
+export OS_AUTH_URL=http://127.0.0.1:35357/v3/
---
+++ devstack/files/schema/influxdb_setup.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# (C) Copyright 2015 Hewlett-Packard Development Company, L.P.
+"""A simple script to setup influxdb user and roles. At some point this should
+   become a more full featured module.  Also this assumes that none of the
+   python based influxdb clients are available on this system.
+"""
+
+ADMIN = 'root'
+ADMIN_PASS = 'root'
+DBNAME = 'mon'
+USERS = {}
+USERS['mon_api'] = 'password'
+USERS['mon_persister'] = 'password'
+
+URL = 'http://127.0.0.1:8086'
+
+SHARDSPACE_NAME = 'persister_all'
+REPLICATION = 1
+RETENTION = '90d'
+
+import json
+import sys
+import time
+import urllib
+import urllib2
+
+
+def influxdb_get(uri, query, db=None):
+    """Runs a query via HTTP GET and returns the response as a Python list."""
+
+    getparams = {"q": query}
+    if db:
+        getparams['db'] = db
+
+    try:
+        params = urllib.urlencode(getparams)
+        uri = "{}&{}".format(uri,params)
+        req = urllib2.urlopen(uri)
+        json_value = json.loads(req.read())
+
+        if (len(json_value['results'][0]) > 0 and
+           'values' in json_value['results'][0]['series'][0]):
+            return json_value['results'][0]['series'][0]['values']
+        else:
+            return []
+
+    except KeyError:
+        print "Query returned a non-successful result: {0}".format(json_value['results'])
+        sys.exit(1)
+
+
+def main(argv=None):
+    """If necessary, create the database, retention policy, and users"""
+    auth_str = '?u=%s&p=%s' % (ADMIN, ADMIN_PASS)
+    api_uri = "{0}/query{1}".format(URL, auth_str)
+
+#    List current databases
+    dbs = influxdb_get(uri=api_uri, query="SHOW DATABASES")
+    if [DBNAME] not in dbs:
+        print "Creating database '{}'".format(DBNAME)
+        influxdb_get(uri=api_uri, query="CREATE DATABASE {0}".format(DBNAME))
+        print "...created!"
+
+#   Check retention policy
+    policies = influxdb_get(uri=api_uri,
+                            query="SHOW RETENTION POLICIES {0}".format(DBNAME))
+    if not any(pol[0] == SHARDSPACE_NAME for pol in policies):
+        # Set retention policy
+        policy = "CREATE RETENTION POLICY {0} ON {1} DURATION {2} REPLICATION {3} DEFAULT".format(SHARDSPACE_NAME,
+                                                                                          DBNAME,
+                                                                                          RETENTION,
+                                                                                          REPLICATION)
+        influxdb_get(uri=api_uri, db=DBNAME, query=policy)
+
+#   Create the users
+    users = influxdb_get(uri=api_uri, query="SHOW USERS", db=DBNAME)
+    for name, password in USERS.iteritems():
+        if not any(user[0] == name for user in users):
+            influxdb_get(uri=api_uri,
+                         query=unicode("CREATE USER {0} WITH PASSWORD '{1}'".format(name, password)),
+                         db=DBNAME)
+
+if __name__ == "__main__":
+    sys.exit(main())
