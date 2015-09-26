2c3693ac39e1094845e4e54294489b0de1ec70f2
Roland Hochmuth <roland.hochmuth@hp.com>
2015-09-26 00:10:43 -0600

    [WIP] Add Monasca Tempest Tests
    Change-Id: I7d1df1b88b63ccc2f1a66deaf439c032b1175d99

---
+++ monasca_tempest_tests/tests/api/test_metrics_names.py
@@ -0,0 +1,31 @@
+# (C) Copyright 2015 Hewlett Packard Enterprise Development Company LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from monasca_tempest_tests.tests.api import base
+from tempest import test
+
+
+class TestMetricsNames(base.BaseMonascaTest):
+
+    @test.attr(type='gate')
+    def test_list_metrics_names(self):
+        return
+
+    @test.attr(type='gate')
+    def test_list_metrics_names_with_dimensions(self):
+        return
+
+    @test.attr(type='gate')
+    def test_list_metrics_names_with_offset_limit(self):
+        return
