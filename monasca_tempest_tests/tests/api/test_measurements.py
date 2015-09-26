a11763cf7297db08aa266a72ee9bb0648b075320
Roland Hochmuth <roland.hochmuth@hp.com>
2015-09-26 00:10:43 -0600

    [WIP] Add Monasca Tempest Tests
    To run the tests:
    
    1. Create a virtualenv for running the Tempest tests and activate it.
    2. Clone the monasca-api, fetch this review, and create a branch.
    3. Install the monasca-api which will also register monasca_tempest_tests.
        python setup.py install
    4. Clone tempest, and cd to it.
    5. Create etc/tempest.conf and etc/logging.conf in the tempest root dir.
    6. Run the tests using testr
    6.1 Create a list of the monasca tests in a file
        testr list-tests monasca_tempest_tests > monasca_tempest_tests
    6.2. Run the tests using testr
        testr run --load-list=monasca_tempest_tests
    7. Run the tests using ostestr
            ostestr --regex monasca_tempest_tests
    
    Change-Id: I7d1df1b88b63ccc2f1a66deaf439c032b1175d99

---
+++ monasca_tempest_tests/tests/api/test_measurements.py
@@ -0,0 +1,86 @@
+# (C) Copyright 2015 Hewlett Packard Enterprise Development Company LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import time
+
+from oslo_utils import timeutils
+
+from monasca_tempest_tests.tests.api import base
+from monasca_tempest_tests.tests.api import helpers
+from tempest import test
+from tempest_lib import exceptions
+
+NUM_MEASUREMENTS = 100
+
+
+class TestMeasurements(base.BaseMonascaTest):
+
+    @classmethod
+    def resource_setup(cls):
+        super(TestMeasurements, cls).resource_setup()
+
+        start_timestamp = int(time.time() * 1000)
+        end_timestamp = int(time.time() * 1000) + NUM_MEASUREMENTS * 1000
+        metrics = []
+
+        for i in xrange(NUM_MEASUREMENTS):
+            metric = helpers.create_metric(
+                name="name-1",
+                timestamp=start_timestamp + i)
+            metrics.append(metric)
+
+        resp, body = cls.monasca_client.create_metrics(metric)
+        cls._start_timestamp = start_timestamp
+        cls._end_timestamp = end_timestamp
+        cls._metrics = metrics
+
+    @test.attr(type="gate")
+    def test_list_measurements(self):
+        start_time = timeutils.iso8601_from_timestamp(
+            self._start_timestamp / 1000)
+        query_parms = '?name=name-1&start_time=' + str(start_time)
+        resp, body = self.monasca_client.list_measurements(query_parms)
+        self.assertEqual(200, resp.status)
+
+    @test.attr(type="gate")
+    def test_list_measurements_with_no_start_time(self):
+        self.assertRaises(exceptions.UnprocessableEntity,
+                          self.monasca_client.list_measurements)
+
+    @test.attr(type="gate")
+    def test_list_measurements_with_name(self):
+        return
+
+    @test.attr(type="gate")
+    def test_list_measurements_with_dimensions(self):
+        return
+
+    @test.attr(type="gate")
+    def test_list_measurements_with_endtime(self):
+        start_time = timeutils.iso8601_from_timestamp(
+            self._start_timestamp / 1000)
+        end_time = timeutils.iso8601_from_timestamp(
+            self._end_timestamp / 1000)
+        query_parms = '?name=name-1&start_time=' + str(start_time) + \
+                      '&end_time=' + str(end_time)
+        resp, body = self.monasca_client.list_measurements(query_parms)
+        self.assertEqual(200, resp.status)
+
+    @test.attr(type="gate")
+    def test_list_measurements_with_offset_limit(self):
+        return
+
+    @test.attr(type="gate")
+    def test_list_measurements_with_merge_metrics(self):
+        return
