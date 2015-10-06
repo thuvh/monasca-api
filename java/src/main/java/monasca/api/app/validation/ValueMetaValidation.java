ba812d36e21d188fdcef365f99cb8d8a226aba67
bklei <bradley.klein@twcable.com>
2015-10-06 09:58:31 -0600

    Check entire set of value_meta key/value pairs for length
    Change-Id: Ie78d1894f87de5c6a7f43703b22406b4ecb17e10
    Closes-Bug: #1501929

--- java/src/main/java/monasca/api/app/validation/ValueMetaValidation.java
+++ java/src/main/java/monasca/api/app/validation/ValueMetaValidation.java
@@ -32,7 +32,11 @@ import javax.ws.rs.WebApplicationException;
  */
 public final class ValueMetaValidation {
   private static final int VALUE_META_MAX_NUMBER = 16;
-  private static final int VALUE_META_VALUE_MAX_LENGTH = 2048;
+  //
+  // To be safe, we're assuming UTF-32 encoding (4 bytes per char), so we'll
+  // set string length to 2048/4.
+  //
+  private static final int VALUE_META_VALUE_MAX_LENGTH = 512;
   private static final int VALUE_META_NAME_MAX_LENGTH = 255;
   private static final Map<String, String> EMPTY_VALUE_META = Collections
       .unmodifiableMap(new HashMap<String, String>());
@@ -90,23 +94,21 @@ public final class ValueMetaValidation {
         throw Exceptions.unprocessableEntity("valueMeta name %s must be %d characters or less",
             name, VALUE_META_NAME_MAX_LENGTH);
       }
-      verifyValueMetaStringLength(name, value);
     }
+    verifyValueMetaStringLength(valueMetas);
   }
 
-  private static void verifyValueMetaStringLength(String name, String value) {
+  private static void verifyValueMetaStringLength(Map<String, String> valueMetas) {
 
-    Map<String, String> tmpMap = new HashMap<String, String>();
-    tmpMap.put(name, value);
     try {
-      String valueMetaString = objectMapper.writeValueAsString(tmpMap);
+      String valueMetaString = objectMapper.writeValueAsString(valueMetas);
 
       if (valueMetaString.length() > VALUE_META_VALUE_MAX_LENGTH) {
-        throw Exceptions.unprocessableEntity("valueMeta name value combination %s must be %d characters or less",
+        throw Exceptions.unprocessableEntity("valueMeta name value combinations %s must be %d characters or less",
           valueMetaString, VALUE_META_VALUE_MAX_LENGTH);
        }
     } catch (JsonProcessingException e) {
-      throw Exceptions.unprocessableEntity("Failed to serialize valueMeta %s", tmpMap);
+      throw Exceptions.unprocessableEntity("Failed to serialize valueMeta combinations %s", valueMetas);
     }
   }
 }
