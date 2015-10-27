e7dbd9211ca2f53dd1ad0b2ecf95f36111810bf8
Deklan Dieterly <deklan.dieterly@hpe.com>
2015-10-27 16:37:39 -0600

    Make devstack-gate work
    Make changes to plugin.sh to get devstack-gate to run.
    
    Change-Id: Ice6c7915c81314e7f286b64c81a065e25f8f5005

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -44,15 +44,15 @@ ERREXIT=$(set +o | grep errexit)
 set -o errexit
 
 # Determine if we are running in devstack-gate or devstack.
-if [[ $BASE ]]; then
+if [[ $DEST ]]; then
 
     # We are running in devstack-gate.
-    export MONASCA_BASE="${BASE}/new"
+    export MONASCA_BASE=${MONASCA_BASE:-"${DEST}"}
 
 else
 
     # We are running in devstack.
-    export MONASCA_BASE="/opt/stack"
+    export MONASCA_BASE=${MONASCA_BASE:-"/opt/stack"}
 
 fi
 
@@ -491,7 +491,7 @@ function install_schema {
 
     sudo chown root:root /opt/monasca/sqls/mon.sql
 
-    sudo mysql -uroot -ppassword < /opt/monasca/sqls/mon.sql || echo "Did the schema change? This process will fail on schema changes."
+    sudo mysql -uroot -p"${MYSQL_PASSWORD}" < /opt/monasca/sqls/mon.sql || echo "Did the schema change? This process will fail on schema changes."
 
     sudo cp -f "${MONASCA_BASE}"/monasca-api/devstack/files/schema/winchester.sql /opt/monasca/sqls/winchester.sql
 
