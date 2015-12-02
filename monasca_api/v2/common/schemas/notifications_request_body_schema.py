461aa625aa937c441ebf0f44d7451fb103a792b2
oiskam1 <oiskam1@yandex.ru>
2015-12-02 10:23:34 +0100

    ORM (sqlalchemy) implementation for python
    It is sqlalchemy part from proposition of oiskam for python.
    
    Change-Id: I699d87c1cd43fe0ab52aa8ca7044f4d90c8a1ba2

--- monasca_api/v2/common/schemas/notifications_request_body_schema.py
+++ monasca_api/v2/common/schemas/notifications_request_body_schema.py
@@ -12,8 +12,9 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from oslo_log import log
 import sys
+
+from oslo_log import log
 from validate_email import validate_email
 import voluptuous
 
