e37da27f203d8da6368de4ea4ed4946bbf62a4ef
Ryan Brandt <ryan.brandt@hp.com>
2015-12-15 15:20:00 -0700

    Allow alarm definition and alarm lists to be sorted
    Change offsets to integers instead of ids
    
    Change-Id: I58712dfe2ae8e6c40f52eb4753fc39e6cbfdd3b9

--- java/src/test/java/monasca/api/infrastructure/persistence/hibernate/AlarmDefinitionSqlRepositoryImplTest.java
+++ java/src/test/java/monasca/api/infrastructure/persistence/hibernate/AlarmDefinitionSqlRepositoryImplTest.java
@@ -49,6 +49,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import javax.ws.rs.WebApplicationException;
+
 @Test(groups = "orm")
 public class AlarmDefinitionSqlRepositoryImplTest {
 
@@ -340,33 +342,38 @@ public class AlarmDefinitionSqlRepositoryImplTest {
       fail();
     } catch (EntityNotFoundException expected) {
     }
-    assertEquals(Arrays.asList(alarmDef_234), repo.find("bob", null, null, null, 1));
+    assertEquals(Arrays.asList(alarmDef_234), repo.find("bob", null, null, null, null, 1));
   }
 
   public void shouldFindByDimension() {
     final Map<String, String> dimensions = new HashMap<>();
     dimensions.put("image_id", "888");
 
-    List<AlarmDefinition> result = repo.find("bob", null, dimensions, null, 1);
+    List<AlarmDefinition> result = repo.find("bob", null, dimensions, null, null, 1);
 
     assertEquals(Arrays.asList(alarmDef_123, alarmDef_234), result);
 
     dimensions.clear();
     dimensions.put("device", "1");
-    assertEquals(Arrays.asList(alarmDef_123), repo.find("bob", null, dimensions, null, 1));
+    assertEquals(Arrays.asList(alarmDef_123), repo.find("bob", null, dimensions, null, null, 1));
 
     dimensions.clear();
     dimensions.put("Not real", "AA");
-    assertEquals(0, repo.find("bob", null, dimensions, null, 1).size());
+    assertEquals(0, repo.find("bob", null, dimensions, null, null, 1).size());
   }
 
   public void shouldFindByName() {
     final Map<String, String> dimensions = new HashMap<>();
     dimensions.put("image_id", "888");
 
-    List<AlarmDefinition> result = repo.find("bob", "90% CPU", dimensions, null, 1);
+    List<AlarmDefinition> result = repo.find("bob", "90% CPU", dimensions, null, null, 1);
 
     assertEquals(Arrays.asList(alarmDef_123), result);
 
+  }
+
+  @Test(groups = "orm", expectedExceptions = WebApplicationException.class)
+  public void shouldFindThrowException() {
+    repo.find("bob", null, null, Arrays.asList("severity", "state"), null, 1);
   }
 }
