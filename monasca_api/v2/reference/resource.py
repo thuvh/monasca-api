bb4a29374317ad358283f2c5b314217e38dacfda
Kaiyan Sheng <kaiyan.sheng@hp.com>
2015-12-16 15:47:35 -0700

    Bug in monasca client for tempest
    Change PATCH to PUT for update alarm and update alarm definitions
    
    Change-Id: I12e9b630dc77dd613a4c72f8547a2babbd793181

--- monasca_api/v2/reference/resource.py
+++ monasca_api/v2/reference/resource.py
@@ -1,5 +1,4 @@
-# Copyright 2014 Hewlett-Packard
-# (C) Copyright 2015 Hewlett Packard Enterprise Development Company LP
+# (C) Copyright 2014-2015 Hewlett Packard Enterprise Development Company LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
