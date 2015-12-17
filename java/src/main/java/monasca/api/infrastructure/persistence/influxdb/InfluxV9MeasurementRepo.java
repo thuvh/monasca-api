61832e6fe8b93fb5821882112458e0ef9aa106be
Kaiyan Sheng <kaiyan.sheng@hp.com>
2015-12-16 17:42:33 -0700

    Bug in return 3 digit timestamp
    Previous code change didnt consider when millisecond=000 condition.
    
    Change-Id: I6af60c7193d51e7ddfb3737ed4db5d1f1c775289

--- java/src/main/java/monasca/api/infrastructure/persistence/influxdb/InfluxV9MeasurementRepo.java
+++ java/src/main/java/monasca/api/infrastructure/persistence/influxdb/InfluxV9MeasurementRepo.java
@@ -131,6 +131,7 @@ public class InfluxV9MeasurementRepo implements MeasurementRepo {
 
   private List<Measurements> measurementsList(Series series) {
     List<Measurements> measurementsList = new LinkedList<>();
+    String timestamp;
 
     if (!series.isEmpty()) {
 
@@ -142,9 +143,13 @@ public class InfluxV9MeasurementRepo implements MeasurementRepo {
 
         for (String[] values : serie.getValues()) {
           final int length = values[0].length();
-          final String millisecond = values[0].substring(20, length-1);
-          final String millisecond_3d = StringUtils.rightPad(millisecond, 3, '0');
-          final String timestamp = values[0].substring(0, 19) + '.' + millisecond_3d + 'Z';
+          if (length == 20) {
+            timestamp = values[0].substring(0, 19) + ".000Z";}
+          else {
+            final String millisecond = values[0].substring(20, length-1);
+            final String millisecond_3d = StringUtils.rightPad(millisecond, 3, '0');
+            timestamp = values[0].substring(0, 19) + '.' + millisecond_3d + 'Z';
+          }
 
           measurements.addMeasurement(
               new Object[]{timestamp, Double.parseDouble(values[1]), getValueMeta(values)});
