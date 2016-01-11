bfad3d03086956dcc793c315ef207c11f9597c75
Kaiyan Sheng <kaiyan.sheng@hp.com>
2016-01-11 10:24:31 -0700

    Add Unit Tests for Java API
    New unit tests for method threeDigitMillisTimestamp() are needed
    since there were bugs found earlier with this new method in
    InfluxV9Utils.java.
    
    Change-Id: I7bd5956f460c5d89ab7fbf56db03de50b36f5e91

--- java/src/test/java/monasca/api/infrastructure/persistence/influxdb/InfluxV9UtilsTest.java
+++ java/src/test/java/monasca/api/infrastructure/persistence/influxdb/InfluxV9UtilsTest.java
@@ -22,7 +22,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
-@Test(groups = "functional")
+@Test
 public class InfluxV9UtilsTest {
 
   private InfluxV9Utils instance;
@@ -60,5 +60,31 @@ public class InfluxV9UtilsTest {
   public void testTimeOffsetPart_0() throws Exception {
     final String offset = "0";
     assertEquals(String.format(" and time > '%1$s'", offset), this.instance.timeOffsetPart(offset));
+  }
+
+  @Test(groups = {"threeDigitMillisTimestamp"})
+  public void testThreeDigitMillisTimestamp_with_3digit() throws Exception {
+    final String origTimestamp = "2016-01-11T16:10:34.472Z";
+    assertEquals(this.instance.threeDigitMillisTimestamp(origTimestamp), origTimestamp);
+  }
+
+  @Test(groups = {"threeDigitMillisTimestamp"})
+  public void testThreeDigitMillisTimestamp_with_2digit() throws Exception {
+    final String origTimestamp_1 = "2016-01-11T16:10:34.47Z";
+    assertEquals(this.instance.threeDigitMillisTimestamp(origTimestamp_1), "2016-01-11T16:10:34.470Z");
+    final String origTimestamp_2 = "2016-01-11T16:10:34.40Z";
+    assertEquals(this.instance.threeDigitMillisTimestamp(origTimestamp_2), "2016-01-11T16:10:34.400Z");
+  }
+
+  @Test(groups = {"threeDigitMillisTimestamp"})
+  public void testThreeDigitMillisTimestamp_with_1digit() throws Exception {
+    final String origTimestamp = "2016-01-11T16:10:34.4Z";
+    assertEquals(this.instance.threeDigitMillisTimestamp(origTimestamp), "2016-01-11T16:10:34.400Z");
+  }
+
+  @Test(groups = {"threeDigitMillisTimestamp"})
+  public void testThreeDigitMillisTimestamp_with_0digit() throws Exception {
+    final String origTimestamp = "2016-01-11T16:10:34Z";
+    assertEquals(this.instance.threeDigitMillisTimestamp(origTimestamp), "2016-01-11T16:10:34.000Z");
   }
 }
