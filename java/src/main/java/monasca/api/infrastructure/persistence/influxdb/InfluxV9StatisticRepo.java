eb5eef0d8bebccd9c500f72c0a80b72f2be80c94
Roland Hochmuth <roland.hochmuth@hp.com>
2016-03-07 20:08:06 -0700

    Add support for multiple metrics in measurements and statistics resources
    Precursor to work on a group_by option for measurements and statistics queries
    
    Change-Id: I6569923687511612c1d9ec318d2c5fe25535f973

--- java/src/main/java/monasca/api/infrastructure/persistence/influxdb/InfluxV9StatisticRepo.java
+++ java/src/main/java/monasca/api/infrastructure/persistence/influxdb/InfluxV9StatisticRepo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ * Copyright (c) 2014, 2106 Hewlett-Packard Development Company, L.P.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -67,7 +67,7 @@ public class InfluxV9StatisticRepo implements StatisticRepo {
   public List<Statistics> find(String tenantId, String name, Map<String, String> dimensions,
                                DateTime startTime, @Nullable DateTime endTime,
                                List<String> statistics, int period, String offset, int limit,
-                               Boolean mergeMetricsFlag) throws Exception {
+                               Boolean mergeMetricsFlag, String groupBy) throws Exception {
 
     String q = buildQuery(tenantId, name, dimensions, startTime, endTime,
                    statistics, period, offset, limit, mergeMetricsFlag);
