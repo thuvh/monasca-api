d32a00e0200b32b115aee06c114aa9c0f30fefb8
Kaiyan Sheng <kaiyan.sheng@hp.com>
2016-03-18 14:24:11 -0600

    Fix statistic tests with Vertica in Devstack
    Set default period equals 300 seconds
    Remove sorting on statistic columns
    Leave dimensions={} if dimension is not in query
    
    Change-Id: I69fb267dd4f46d331e9e4110e3bae40436f7f3d3

--- java/src/main/java/monasca/api/infrastructure/persistence/vertica/StatisticVerticaRepoImpl.java
+++ java/src/main/java/monasca/api/infrastructure/persistence/vertica/StatisticVerticaRepoImpl.java
@@ -133,7 +133,7 @@ public class StatisticVerticaRepoImpl implements StatisticRepo {
 
       statistics.setColumns(statisticsColumns);
 
-      if (Boolean.TRUE.equals(mergeMetricsFlag) && byteMap.keySet().size() > 1) {
+      if (Boolean.TRUE.equals(mergeMetricsFlag)) {
 
         // Wipe out the dimensions.
         statistics.setDimensions(new HashMap<String, String>());
@@ -293,6 +293,10 @@ public class StatisticVerticaRepoImpl implements StatisticRepo {
     StringBuilder sb = new StringBuilder();
 
     sb.append("SELECT " + createColumnsStr(statistics));
+
+    if (period <= 0) {
+      period = 300;
+    }
 
     if (period >= 1) {
       sb.append("Time_slice(time_stamp, " + period);
