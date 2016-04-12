36184c83efd06f259bbb504d2b919e8443658d17
Kaiyan Sheng <kaiyan.sheng@hp.com>
2016-04-12 10:33:52 -0600

    Fix monascaclient in tempest for alarm def update
    Monasca client in tempest for alarm-definition-update was wrong. All
    the arguments for update alarm definition should be required.
    
    Change-Id: I4c38150c2130715f200ec044d24210bfb589d1f4

--- java/src/main/java/monasca/api/app/AlarmDefinitionService.java
+++ java/src/main/java/monasca/api/app/AlarmDefinitionService.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -93,7 +93,7 @@ public class AlarmDefinitionService {
   /**
    * Creates an alarm definition and publishes an AlarmDefinitionCreatedEvent. Note, the event is
    * published first since chances of failure are higher.
-   * 
+   *
    * @throws EntityExistsException if an alarm already exists for the name
    * @throws InvalidEntityException if one of the actions cannot be found
    */
@@ -143,7 +143,7 @@ public class AlarmDefinitionService {
 
   /**
    * Deletes the alarm definition identified by the {@code alarmDefId}.
-   * 
+   *
    * @throws EntityNotFoundException if the alarm cannot be found
    */
   public void delete(String tenantId, String alarmDefId) {
@@ -242,7 +242,7 @@ public class AlarmDefinitionService {
   /**
    * Patches the alarm definition for the {@code tenantId} and {@code alarmDefId} to the state of
    * the {@code fields}.
-   * 
+   *
    * @throws EntityNotFoundException if the alarm cannot be found
    * @throws InvalidEntityException if one of the actions cannot be found
    */
