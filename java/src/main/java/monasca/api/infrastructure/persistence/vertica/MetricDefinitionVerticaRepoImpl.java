61afde4a59e19d4704d623f9f5c13e40cd5ad7eb
Kaiyan Sheng <kaiyan.sheng@hp.com>
2016-07-29 16:49:42 -0600

    Return 422 exception when querying vertica with wrong timestamp
    CLI returns internal server error when input timestamp is in the
    wrong format(for example 20160729T12:00:00Z). ISO_8601_FORMATTER
    parse the timestamp into 20160729-01-01T12:00:00.000Z in this case.
    The internal server error came from vertica when vertica tried to
    query with the out of range timestamp. Adding the try catch in
    vertica code to return 422 unprocessable_entity instead.
    
    Change-Id: Ie70e8d725ec00a93fb1fb82b8f4e7a24140dcdbb

--- java/src/main/java/monasca/api/infrastructure/persistence/vertica/MetricDefinitionVerticaRepoImpl.java
+++ java/src/main/java/monasca/api/infrastructure/persistence/vertica/MetricDefinitionVerticaRepoImpl.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2014,2016 Hewlett Packard Enterprise Development Company LP
+ * (C) Copyright 2014,2016 Hewlett Packard Enterprise Development LP
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -301,7 +301,9 @@ public class MetricDefinitionVerticaRepoImpl implements MetricDefinitionRepo {
 
       MetricQueries.bindDimensionsToQuery(query, dimensions);
 
-      return query.list();
+      List<Map<String, Object>> rows;
+      rows = VerticaUtils.queryList(query);
+      return rows;
 
     }
   }
