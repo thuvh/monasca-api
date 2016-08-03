b921ae49922c80786f0d1f12f2e95d596131691a
Kaiyan Sheng <kaiyan.sheng@hp.com>
2016-08-03 17:52:24 -0600

    Add metrics/dimensions/names into monasca-api
    This endpoint will return all the dimension names for a given metric
    name.
    Added dimension-names for influx in python api and java api
    Added dimension-names for vertica in java api
    
    Change-Id: I0192ccb9276ea94103a477bd2ad7d10f21e64d31
    Implements: blueprint dimensions-api

---
+++ java/src/main/java/monasca/api/domain/model/dimension/DimensionBase.java
@@ -0,0 +1,75 @@
+/*
+ * (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package monasca.api.domain.model.dimension;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import monasca.common.model.domain.common.AbstractEntity;
+
+/**
+ * Base function for DimensionNames and DimensionValues.
+ */
+public class DimensionBase extends AbstractEntity {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String metricName;
+    private List<String> dimensionInfo;
+
+    public DimensionBase(String metricName, List<String> dimensionInfo) {
+        this.metricName = metricName;
+        this.dimensionInfo = dimensionInfo;
+    }
+
+    public List<String> getDimensionInfo() {
+        return dimensionInfo;
+    }
+
+    public String getMetricName() {
+        return metricName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DimensionBase other = (DimensionBase) obj;
+        if (metricName == null) {
+            if (other.getMetricName() != null)
+                return false;
+        } else if (!metricName.equals(other.getMetricName()))
+            return false;
+        if (dimensionInfo == null) {
+            if (other.getDimensionInfo() != null)
+                return false;
+        } else if (!dimensionInfo.equals(other.getDimensionInfo()))
+            return false;
+        return true;
+    }
+
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((metricName == null) ? 0 : metricName.hashCode());
+        result = prime * result + ((dimensionInfo == null) ? 0 : dimensionInfo.hashCode());
+        return result;
+    }
+}
---
+++ java/src/main/java/monasca/api/domain/model/dimension/DimensionNames.java
@@ -0,0 +1,49 @@
+/*
+ * (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package monasca.api.domain.model.dimension;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ * Encapsulates the list of dimension names for an optional metric name.
+ */
+public class DimensionNames extends DimensionBase {
+
+    public DimensionNames(String metricName, List<String> dimensionNames) {
+        super(metricName, dimensionNames);
+        this.id = generateId();
+    }
+
+    @JsonProperty("dimensionNames")
+    public List<String> getDimensionInfo() {
+        return super.getDimensionInfo();
+    }
+
+    private String generateId() {
+        String hashstr = "metricName=" + getMetricName();
+        byte[] sha1Hash = DigestUtils.sha(hashstr);
+        return Hex.encodeHexString(sha1Hash);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("DimensionNames: MetricName=%s DimensionNames [names=%s]",
+                getMetricName(), getDimensionInfo());
+    }
+}
