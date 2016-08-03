a6fae4c94abcb49a4a885171c69af044683df72e
Kaiyan Sheng <kaiyan.sheng@hp.com>
2016-08-03 17:52:24 -0600

    Add metrics/dimensions/names into monasca-api
    This endpoint will return all the dimension names for a given metric
    name.
    Added dimension-names for influx in python api and java api
    Added dimension-names for vertica in java api
    
    Change-Id: I0192ccb9276ea94103a477bd2ad7d10f21e64d31
    Implements: blueprint dimensions-api

--- monasca_api/tests/test_repositories.py
+++ monasca_api/tests/test_repositories.py
@@ -1,4 +1,5 @@
 # Copyright 2015 Cray Inc. All Rights Reserved.
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -163,6 +164,41 @@ class TestRepoMetricsInfluxDB(unittest.TestCase):
             ],
             u'id': 'bea9565d854a16a3366164de213694c190f27675',
             u'metric_name': 'custom_metric'
+        })
+
+    @patch("monasca_api.common.repositories.influxdb.metrics_repository.client.InfluxDBClient")
+    def test_list_dimension_names(self, influxdb_client_mock):
+        mock_client = influxdb_client_mock.return_value
+        mock_client.query.return_value.raw = {
+            u'series': [{
+                u'values': [[
+                    u'custom_metric,_region=useast,_tenant_id=38dc2a2549f94d2e9a4fa1cc45a4970c,'
+                    u'hostname=custom_host,service=custom_service',
+                    u'useast',
+                    u'38dc2a2549f94d2e9a4fa1cc45a4970c',
+                    u'custom_host',
+                    u'custom_service'
+                ]],
+                u'name': u'custom_metric',
+                u'columns': [u'_key', u'_region', u'_tenant_id', u'hostname', u'service']
+            }]
+        }
+
+        repo = influxdb_repo.MetricsRepository()
+
+        result = repo.list_dimension_names(
+            "38dc2a2549f94d2e9a4fa1cc45a4970c",
+            "useast",
+            "custom_metric",
+            offset=None,
+            limit=1)
+
+        self.assertEqual(result, {
+            u'dimension_names': [
+                u'hostname',
+                u'service'],
+            u'id': '41aa72d77251af78467dd82a591281d357cbf62b',
+            u'metric_name': u'custom_metric',
         })
 
 
