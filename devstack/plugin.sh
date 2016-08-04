9f96337b04f2108d78868083be7b44c141fe1498
Andrea Adams <aadams@hpe.com>
2016-08-04 14:52:15 -0600

    Devstack should apply changes to affected repos
    Devstack check job didn't apply any monascastatsd changes to the
    monasca-notification repo which would cause failures. It now checks
    for monasca-statsd changes when installing monasca-notification.
    
    Change-Id: I26f9c588f2818059ab6ba24f9fad8e213798a39c

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1190,6 +1190,16 @@ function install_monasca_notification {
     sudo apt-get -y install python-mysqldb
     sudo apt-get -y install libmysqlclient-dev
 
+    if [[ ! -d "${MONASCA_BASE}"/monasca-statsd ]]; then
+
+        sudo git clone https://git.openstack.org/openstack/monasca-statsd "${MONASCA_BASE}"/monasca-statsd
+
+    fi
+
+    (cd "${MONASCA_BASE}"/monasca-statsd ; sudo python setup.py sdist)
+
+    MONASCA_STATSD_SRC_DIST=$(ls -td "$MONASCA_BASE"/monasca-statsd/dist/monasca-statsd*.tar.gz | head -1)
+
     if [[ ! -d "${MONASCA_BASE}"/monasca-notification ]]; then
 
         sudo git clone https://git.openstack.org/openstack/monasca-notification "${MONASCA_BASE}"/monasca-notification
@@ -1202,7 +1212,12 @@ function install_monasca_notification {
 
     PIP_VIRTUAL_ENV=/opt/monasca
 
+    pip_install $MONASCA_STATSD_SRC_DIST
+
     pip_install --allow-unverified simport $MONASCA_NOTIFICATION_SRC_DIST
+
+    # Debug line ~ Need to Remove ~ #
+    (cd $PIP_VIRTUAL_ENV ; ./bin/pip list)
 
     pip_install mysql-python
 
