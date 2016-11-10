bf45b73274aa7123946eac40bcd8859e29d4e816
Roland Hochmuth <roland.hochmuth@hp.com>
2016-11-09 21:31:57 -0700

    Add support for InfluxDB 0.11.0 to 1.1.0 in the Python API
    In InfluxDB version 0.11.0 the result set for SHOW SERIES changed. See
    
    https://github.com/influxdata/influxdb/blob/master/CHANGELOG.md#v0110-2016-03-22
    
    Modifed the code to be compatible with either the old or new result
    set.
    
    In InfluxDB version 1.0.0 the config file shanged. See
    
    https://github.com/influxdata/influxdb/blob/master/CHANGELOG.md#v100-2016-09-08
    
    Modified the config file deployed by DevStack to be compatible with either the
    old or new config file.
    
    In some other InfluxDB release several database ops, such as CREATE
    DATABASE, were modified from http GET to POST. Modified how databases
    are created in the DevStack plugin.sh.
    
    Change-Id: I33188f4e64150d2cb24c0a06979c06f03c803da4

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -51,6 +51,23 @@ export MONASCA_PERSISTER_IMPLEMENTATION_LANG=${MONASCA_PERSISTER_IMPLEMENTATION_
 # Set default metrics DB to InfluxDB
 export MONASCA_METRICS_DB=${MONASCA_METRICS_DB:-influxdb}
 
+# Set INFLUXDB_VERSION
+if [[ "${MONASCA_API_IMPLEMENTATION_LANG,,}" == 'java' ]]; then
+
+    INFLUXDB_VERSION=${INFLUXDB_VERSION:-${INFLUXDB_JAVA_VERSION}}
+
+elif [[ "${MONASCA_API_IMPLEMENTATION_LANG,,}" == 'python' ]]; then
+
+    INFLUXDB_VERSION=${INFLUXDB_VERSION:-${INFLUXDB_PYTHON_VERSION}}
+
+else
+
+    echo "Found invalid value for variable MONASCA_API_IMPLEMENTATION_LANG: $MONASCA_API_IMPLEMENTATION_LANG"
+    echo "Valid values for MONASCA_API_IMPLEMENTATION_LANG are \"java\" and \"python\""
+    die "Please set MONASCA_API_IMPLEMENTATION_LANG to either \"java'' or \"python\""
+
+fi
+
 # Determine password for database (copied from devstack/lib/database)
 if [ -n "$MYSQL_PASSWORD" ]; then
     DATABASE_PASSWORD=$MYSQL_PASSWORD
@@ -59,6 +76,43 @@ fi
 # db users
 MON_DB_USERS=("notification" "monapi" "thresh")
 MON_DB_HOSTS=("%" "localhost" "$MYSQL_HOST")
+
+# Compares two program version strings.
+# Returns "lt" if $1 is less than $2, "eq" if equal, and "gt" if greater than
+function compare_versions {
+    if [[ $1 == $2 ]]
+    then
+        echo eq
+        return
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            echo gt
+            return
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            echo lt
+            return
+        fi
+    done
+    echo eq
+    return
+}
 
 function pre_install_monasca {
 :
@@ -470,13 +524,21 @@ function install_monasca_influxdb {
     sudo mkdir -p /opt/monasca_download_dir || true
 
     if [[ "$OFFLINE" != "True" ]]; then
-        sudo curl http://s3.amazonaws.com/influxdb/influxdb_${INFLUXDB_VERSION}_amd64.deb \
+        sudo curl https://dl.influxdata.com/influxdb/releases/influxdb_${INFLUXDB_VERSION}_amd64.deb \
             -o /opt/monasca_download_dir/influxdb_${INFLUXDB_VERSION}_amd64.deb
     fi
 
     sudo dpkg --skip-same-version -i /opt/monasca_download_dir/influxdb_${INFLUXDB_VERSION}_amd64.deb
 
-    sudo cp -f "${MONASCA_API_DIR}"/devstack/files/influxdb/influxdb.conf /etc/influxdb/influxdb.conf
+    # In InfluxDB v1.0.0 the config options cluster, collectd and opentsdb changed. As a result
+    # a different config file is deployed. See,
+    # https://github.com/influxdata/influxdb/blob/master/CHANGELOG.md#v100-2016-09-08, for more details.
+    retval=$(compare_versions ${INFLUXDB_VERSION} "1.0.0")
+    if [[ "$retval" == "lt" ]]; then
+        sudo cp -f "${MONASCA_API_DIR}"/devstack/files/influxdb/influxdb.conf /etc/influxdb/influxdb.conf
+    else
+        sudo cp -f "${MONASCA_API_DIR}"/devstack/files/influxdb/influxdb-1.0.0.conf /etc/influxdb/influxdb.conf
+    fi
 
     if [[ ${SERVICE_HOST} ]]; then
 
