ab045a356e3c54ca0980d3147df90ca2da37e9c4
Shinya Kawabata <s-kawabata@wx.jp.nec.com>
2016-11-11 20:33:27 +0900

    Add multibyte character support for alarm definition
    It was failed that putting multibyte character into
    alarm definition name and description by monascaclient.
    There were some mistakes about treatment of utf8 encoding.
    And mysql connection had no utf8 option, so mysql could not
    handle multibyte character.
    
    Change-Id: I8743f89fcc5d5efd4e50f440b76d78abc037e8e7

--- monasca_api/v2/reference/alarm_definitions.py
+++ monasca_api/v2/reference/alarm_definitions.py
@@ -266,7 +266,7 @@ class AlarmDefinitions(alarm_definitions_api_v2.AlarmDefinitionsV2API,
         description = (alarm_definition_row['description']
                        if alarm_definition_row['description'] is not None else None)
 
-        expression = alarm_definition_row['expression'].decode('utf8')
+        expression = alarm_definition_row['expression']
         is_deterministic = is_definition_deterministic(expression)
 
         result = {
@@ -277,11 +277,11 @@ class AlarmDefinitions(alarm_definitions_api_v2.AlarmDefinitionsV2API,
             u'description': description,
             u'expression': expression,
             u'deterministic': is_deterministic,
-            u'id': alarm_definition_row['id'].decode('utf8'),
+            u'id': alarm_definition_row['id'],
             u'match_by': match_by,
-            u'name': alarm_definition_row['name'].decode('utf8'),
-            u'severity': alarm_definition_row['severity'].decode(
-                'utf8').upper()}
+            u'name': alarm_definition_row['name'],
+            u'severity': alarm_definition_row['severity'].upper()
+        }
 
         return result
 
