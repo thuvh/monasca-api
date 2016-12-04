cc6d0bdf3bab4744eeca9bc843bdda0cc4d9ab07
gengchc2 <geng.changcai2@zte.com.cn>
2016-12-04 13:16:43 +0800

    Delete deprecated Hacking in tox.ini
    Some hacking have been removed,so we can delete them.
    More details:
        https://github.com/openstack-dev/hacking/blob/master/setup.cfg
    
    
    Change-Id: I0f4c21d4aaeb1f273a3412886c03566e8fc8bf58

--- monasca_api/common/repositories/influxdb/metrics_repository.py
+++ monasca_api/common/repositories/influxdb/metrics_repository.py
@@ -509,7 +509,7 @@ class MetricsRepository(metrics_repository.AbstractMetricsRepository):
 
                 LOG.exception(ex)
 
-                raise ex
+                raise
 
         except InfluxDBClientError as ex:
 
