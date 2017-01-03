a7884bc102f248c78a7bfea341696d1da5c6b262
Ryan Brandt <ryan.brandt@hp.com>
2017-01-03 12:03:34 -0700

    Use monasca_common for metric validation
    Depends-On: I91536f28ca3d58f1002eed5a7631b6c4c377b6b0
    Depends-On: Ibabff76b3f1592334c281f57a1a5b939bb11e1f8
    Change-Id: I40ce762a9ede88069f759819fe86aedf63476ab8

--- monasca_api/v2/reference/helpers.py
+++ monasca_api/v2/reference/helpers.py
@@ -17,6 +17,7 @@ import datetime
 import json
 
 import falcon
+import monasca_common.validation.metrics as metric_validation
 from oslo_log import log
 from oslo_utils import timeutils
 import simplejson
@@ -24,9 +25,6 @@ import six
 import six.moves.urllib.parse as urlparse
 
 from monasca_api.v2.common.exceptions import HTTPUnprocessableEntityError
-from monasca_api.v2.common.schemas import dimensions_schema
-from monasca_api.v2.common.schemas import exceptions as schemas_exceptions
-from monasca_api.v2.common.schemas import metric_name_schema
 
 LOG = log.getLogger(__name__)
 
@@ -284,9 +282,11 @@ def validate_query_name(name):
     :param name: Query param name.
     :raises falcon.HTTPBadRequest: If name is not valid.
     """
+    if not name:
+        return
     try:
-        metric_name_schema.validate(name)
-    except schemas_exceptions.ValidationException as ex:
+        metric_validation.validate_name(name)
+    except Exception as ex:
         LOG.debug(ex)
         raise HTTPUnprocessableEntityError('Unprocessable Entity', ex.message)
 
@@ -298,10 +298,20 @@ def validate_query_dimensions(dimensions):
     :raises falcon.HTTPBadRequest: If dimensions are not valid.
     """
     try:
-        dimensions_schema.validate(dimensions)
-    except schemas_exceptions.ValidationException as ex:
+        assert isinstance(dimensions, list)
+        for dimension in dimensions:
+            name_value = dimension.split(':')
+            metric_validation.validate_dimension_key(name_value[0])
+            if len(name_value) > 1:
+                if '|' in name_value[1]:
+                    values = name_value[1].split('|')
+                    for value in values:
+                        metric_validation.validate_dimension_value(name_value[0], value)
+                else:
+                    metric_validation.validate_dimension_value(name_value[0], name_value[1])
+    except Exception as ex:
         LOG.debug(ex)
-        raise HTTPUnprocessableEntityError('Unprocessable Entity', ex.message)
+        raise HTTPUnprocessableEntityError("Unprocessable Entity", ex.message)
 
 
 def paginate(resource, uri, limit):
