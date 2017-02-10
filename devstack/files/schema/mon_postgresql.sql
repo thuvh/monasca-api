6b1345d09510c253da7e131606ba1229a5705b89
Kaiyan Sheng <kaiyan.sheng@hp.com>
2017-02-10 16:58:04 -0700

    [WIP] Add API Resource for Alarm Group Definition
    Alarm grouping is used to categorize alarms of similar nature into
    a single notification. This functionality is very usefully
    especially during large outages when many systems fail at once and
    thousands of alarms go off simultaneously.
    
    Change-Id: I02a4333350d5d84598f0fd3442aa2af025a4baeb
    Partially-Implements: blueprint alarm-manager-resource
    Story: 2000935
    Task: 4059

--- devstack/files/schema/mon_postgresql.sql
+++ devstack/files/schema/mon_postgresql.sql
@@ -1,5 +1,6 @@
 ---
 -- # Copyright 2017 FUJITSU LIMITED
+-- # (C) Copyright 2017 Hewlett Packard Enterprise Development LP
 ---
 
 SET statement_timeout = 0;
@@ -128,6 +129,28 @@ CREATE TABLE sub_alarm_definition_dimension (
     sub_alarm_definition_id character varying(36) NOT NULL
 );
 
+CREATE TABLE alarm_group_definition (
+    id character varying(36) NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    deleted_at timestamp without time zone,
+    name character varying(255) NOT NULL,
+    matchers character varying(255) NOT NULL,
+    group_wait character varying(10) NOT NULL,
+    repeat_interval character varying(10) NOT NULL,
+    tenant_id character varying(36) NOT NULL
+);
+
+CREATE TABLE alarm_group_definition_action (
+    alarm_group_definition_id character varying(36) NOT NULL,
+    action_id character varying(36) NOT NULL
+);
+
+CREATE TABLE alarm_group_definition_exclusion (
+    alarm_group_definition_id character varying(36) NOT NULL,
+    exclusion_name character varying(36) NOT NULL,
+    value character varying(36) NOT NULL
+);
 ---
 -- primary keys
 ---
@@ -161,6 +184,13 @@ ALTER TABLE ONLY sub_alarm_definition
 ALTER TABLE ONLY sub_alarm
     ADD CONSTRAINT sub_alarm_pkey PRIMARY KEY (id);
 
+ALTER TABLE ONLY alarm_group_definition
+    ADD CONSTRAINT alarm_group_definition_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY alarm_group_definition_action
+    ADD CONSTRAINT alarm_group_definition_action_pkey PRIMARY KEY
+    (alarm_group_definition_id, action_id);
+
 ---
 -- indexes
 ---
@@ -177,6 +207,15 @@ CREATE INDEX tenant_id ON alarm_definition USING btree (tenant_id);
 ---
 -- foreign key constraints
 ---
+ALTER TABLE ONLY alarm_group_definition_action
+    ADD CONSTRAINT fk_action_alarm_group_definition_id FOREIGN KEY
+    (alarm_group_definition_id)
+    REFERENCES alarm_group_definition(id);
+
+ALTER TABLE ONLY alarm_group_definition_exclusion
+    ADD CONSTRAINT fk_exclusion_alarm_group_definition_id FOREIGN KEY (alarm_group_definition_id)
+    REFERENCES alarm_group_definition(id);
+
 ALTER TABLE ONLY alarm_action
     ADD CONSTRAINT fk_alarm_action_alarm_definition FOREIGN KEY (alarm_definition_id) REFERENCES alarm_definition(id);
 
