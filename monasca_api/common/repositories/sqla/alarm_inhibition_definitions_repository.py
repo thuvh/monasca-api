7f41b7bc9317ab67669a4279c98fb0469f2fbac2
Kaiyan Sheng <kaiyan.sheng@hp.com>
2017-02-17 17:38:19 -0700

    [WIP] Add API Resource for Alarm Inhibition Definition
    Inhibition allows user to mute a set of alarms (target alarms)
    given that another alarm (source alarm) is firing. For example,
    we can use this to mute any low level alarms if the same alarm is
    already critical.
    
    Change-Id: I8c5b9e6bec568507fb9c2ce2e690774ec0838ab0
    Partially-Implements: blueprint alarm-manager-resource

---
+++ monasca_api/common/repositories/sqla/alarm_inhibition_definitions_repository.py
@@ -0,0 +1,314 @@
+# Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import datetime
+
+from oslo_log import log
+from oslo_utils import uuidutils
+
+from monasca_api.common.repositories import \
+    alarm_inhibition_definitions_repository as aidr
+from monasca_api.common.repositories import exceptions
+from monasca_api.common.repositories.sqla import models
+from monasca_api.common.repositories.sqla import sql_repository
+from sqlalchemy import MetaData, insert, update, delete
+from sqlalchemy import select, text, bindparam, null
+
+LOG = log.getLogger(__name__)
+
+
+class AlarmInhibitionDefinitionsRepository(sql_repository.SQLRepository, aidr.AlarmInhibitionDefinitionsRepository):
+    def __init__(self):
+        super(AlarmInhibitionDefinitionsRepository, self).__init__()
+
+        metadata = MetaData()
+        self.aid = models.create_aid_model(metadata)
+        self.aid_sm = models.create_aidsm_model(metadata)
+        self.aid_tm = models.create_aidtm_model(metadata)
+
+        aid = self.aid
+        aid_sm = self.aid_sm
+        aid_tm = self.aid_tm
+        aid_s = aid.alias('aid')
+        self.aid_s = aid_s
+
+        aidsm_columns = [aid_sm.c.source_name + text("'='") +
+                         aid_sm.c.source_value]
+        aidtm_columns = [aid_tm.c.target_name + text("'='") +
+                         aid_tm.c.target_value]
+
+        aidsmg = (
+            select([aid_sm.c.alarm_inhibition_definition_id, models.group_concat(
+                aidsm_columns).label('source_match')]).select_from(aid_sm).
+            group_by(aid_sm.c.alarm_inhibition_definition_id).alias('aidsmg'))
+
+        aidtmg = (select([aid_tm.c.alarm_inhibition_definition_id,
+                          models.group_concat(aidtm_columns).label(
+                              'target_match')])
+                  .select_from(aid_tm)
+                  .group_by(aid_tm.c.alarm_inhibition_definition_id).alias(
+            'aidtmg'))
+
+        self.base_query_from = (aid_s.outerjoin(
+            aidsmg, aidsmg.c.alarm_inhibition_definition_id ==
+            aid_s.c.id).outerjoin(
+            aidtmg, aidtmg.c.alarm_inhibition_definition_id ==
+            aid_s.c.id))
+
+        self.base_query = (select([aid_s.c.id,
+                                   aid_s.c.name,
+                                   aid_s.c.equal,
+                                   aidsmg.c.source_match,
+                                   aidtmg.c.target_match]))
+
+        self.insert_aid_query = \
+            (insert(aid).values(id=bindparam('b_id'),
+                                tenant_id=bindparam('b_tenant_id'),
+                                name=bindparam('b_name'),
+                                equal=bindparam('b_equal'),
+                                created_at=bindparam('b_created_at'),
+                                updated_at=bindparam('b_updated_at')))
+
+        b_aid_id = bindparam('b_alarm_inhibition_definition_id')
+        self.insert_aidsm_query = \
+            (insert(aid_sm).values(alarm_inhibition_definition_id=b_aid_id,
+                                   source_name=bindparam('b_source_name'),
+                                   source_value=bindparam('b_source_value')))
+
+        self.insert_aidtm_query = \
+            (insert(aid_tm).values(alarm_inhibition_definition_id=b_aid_id,
+                                   target_name=bindparam('b_target_name'),
+                                   target_value=bindparam('b_target_value')))
+
+        self.update_aid_query = (
+            update(aid).where(aid.c.tenant_id == bindparam('b_tenant_id'))
+                       .where(aid.c.id == bindparam('b_id')))
+
+        self.delete_aidsm_query = (
+            delete(aid_sm)
+            .where(aid_sm.c.alarm_inhibition_definition_id == bindparam(
+                'b_alarm_inhibition_definition_id')))
+
+        self.delete_aidtm_query = (
+            delete(aid_tm)
+            .where(aid_tm.c.alarm_inhibition_definition_id == bindparam(
+                'b_alarm_inhibition_definition_id')))
+
+        self.soft_delete_aid_query = (update(aid).where(
+            aid.c.tenant_id == bindparam('b_tenant_id'))
+            .where(aid.c.id == bindparam('b_id'))
+            .where(aid.c.deleted_at == null())
+            .values(deleted_at=datetime.datetime.utcnow()))
+
+    @sql_repository.sql_try_catch_block
+    def create_alarm_inhibition_definition(self, tenant_id, name, equal,
+                                           source_match, target_match):
+        with self._db_engine.begin() as conn:
+            now = datetime.datetime.utcnow()
+            aid_id = uuidutils.generate_uuid()
+
+            conn.execute(self.insert_aid_query,
+                         b_id=aid_id,
+                         b_tenant_id=tenant_id,
+                         b_name=name.encode('utf8'),
+                         b_equal=",".join(equal).encode('utf8'),
+                         b_created_at=now,
+                         b_updated_at=now)
+
+            self._insert_into_source_match(conn, aid_id, source_match)
+
+            self._insert_into_target_match(conn, aid_id, target_match)
+
+            return aid_id
+
+    @sql_repository.sql_try_catch_block
+    def get_alarm_inhibition_definitions(self, tenant_id, name=None,
+                                         offset=None, limit=1000):
+        with self._db_engine.connect() as conn:
+            aid = self.aid_s
+            query_from = self.base_query_from
+            params = {'b_tenant_id': tenant_id}
+            query = (self.base_query
+                     .select_from(query_from)
+                     .where(aid.c.tenant_id == bindparam('b_tenant_id'))
+                     .where(aid.c.deleted_at == null()))
+            if name:
+                query = query.where(aid.c.name == bindparam('b_name'))
+                params['b_name'] = name.encode('utf8')
+
+            if offset:
+                query = query.offset(bindparam('b_offset'))
+                params['b_offset'] = offset
+
+            order_columns = [aid.c.id]
+            query = query.order_by(*order_columns)
+            query = query.limit(bindparam('b_limit'))
+            params['b_limit'] = limit
+
+            return [dict(row) for row in conn.execute(query, params).fetchall()]
+
+    @sql_repository.sql_try_catch_block
+    def get_alarm_inhibition_definition(self, tenant_id, _id):
+        with self._db_engine.connect() as conn:
+            return self._get_alarm_inhibition_definition(conn, tenant_id, _id)
+
+    @sql_repository.sql_try_catch_block
+    def update_or_patch_alarm_inhibition_definition(
+            self, tenant_id, aid_id, name,
+            equal, source_match, target_match, patch=False):
+
+        with self._db_engine.begin() as conn:
+            original_row = self._get_alarm_inhibition_definition(
+                conn, tenant_id, aid_id)
+
+            # Get a common update time
+            now = datetime.datetime.utcnow()
+
+            if name is None:
+                new_name = original_row['name']
+            else:
+                new_name = name.encode('utf8')
+
+            if equal is None:
+                new_equal = original_row['equal']
+            else:
+                new_equal = ",".join(equal).encode('utf8')
+
+            conn.execute(
+                self.update_aid_query
+                    .values(name=bindparam('b_name'),
+                            equal=bindparam('b_equal'),
+                            updated_at=bindparam('b_updated_at')),
+                b_name=new_name,
+                b_equal=new_equal,
+                b_updated_at=now,
+                b_tenant_id=tenant_id,
+                b_id=aid_id)
+
+            # Delete old source_match
+            if patch:
+                if source_match is not None:
+                    self._delete_alarm_inhibition_definition_source_match(
+                        conn, aid_id)
+            else:
+                conn.execute(self.delete_aidsm_query,
+                             b_alarm_inhibition_definition_id=aid_id)
+
+            # Insert new source_match
+            self._insert_into_source_match(conn, aid_id, source_match)
+
+            # Delete old target_match
+            if patch:
+                if target_match is not None:
+                    self._delete_alarm_inhibition_definition_target_match(
+                        conn, aid_id)
+            else:
+                conn.execute(
+                    self.delete_aidtm_query,
+                    b_alarm_inhibition_definition_id=aid_id)
+
+            # Insert new target_match
+            self._insert_into_target_match(
+                conn, aid_id, target_match)
+
+            aid = self.aid_s
+            query = (self.base_query
+                     .select_from(self.base_query_from)
+                     .where(aid.c.tenant_id == bindparam('b_tenant_id'))
+                     .where(aid.c.id == bindparam('b_id'))
+                     .where(aid.c.deleted_at == null()))
+
+            updated_row = conn.execute(query,
+                                       b_id=aid_id,
+                                       b_tenant_id=tenant_id).fetchone()
+
+            if updated_row is None:
+                raise Exception("Failed to find current alarm inhibition "
+                                "definition")
+
+            # Return the alarm inhibition definition
+            return updated_row
+
+    @sql_repository.sql_try_catch_block
+    def delete_alarm_inhibition_definition(self, tenant_id,
+                                           aid_id):
+        """Soft delete the alarm inhibition definition.
+        """
+
+        with self._db_engine.begin() as conn:
+            cursor = conn.execute(self.soft_delete_aid_query,
+                                  b_tenant_id=tenant_id,
+                                  b_id=aid_id)
+
+            if cursor.rowcount < 1:
+                return False
+
+            return True
+
+    def _insert_into_source_match(
+            self, conn, aid_id, source_match):
+        if source_match is None:
+            return
+
+        source_match_list = [[key, value] for key, value in
+                             source_match.items()]
+        for source_match in source_match_list:
+            query = self.insert_aidsm_query
+            source_name = source_match[0].encode('utf8')
+            source_value = source_match[1].encode('utf8')
+            conn.execute(query,
+                         b_alarm_inhibition_definition_id=aid_id,
+                         b_source_name=source_name,
+                         b_source_value=source_value)
+
+    def _insert_into_target_match(
+            self, conn, aid_id, target_match):
+        if target_match is None:
+            return
+
+        target_match_list = [[key, value] for key, value in
+                             target_match.items()]
+        for target_match in target_match_list:
+            query = self.insert_aidtm_query
+            target_name = target_match[0].encode('utf8')
+            target_value = target_match[1].encode('utf8')
+            conn.execute(query,
+                         b_alarm_inhibition_definition_id=aid_id,
+                         b_target_name=target_name,
+                         b_target_value=target_value)
+
+    def _get_alarm_inhibition_definition(self, conn, tenant_id, _id):
+        aid = self.aid_s
+        query = (self.base_query
+                 .select_from(self.base_query_from)
+                 .where(aid.c.tenant_id == bindparam('b_tenant_id'))
+                 .where(aid.c.id == bindparam('b_id'))
+                 .where(aid.c.deleted_at == null()))
+
+        row = conn.execute(query,
+                           b_tenant_id=tenant_id,
+                           b_id=_id).fetchone()
+
+        if row is not None:
+            return dict(row)
+        else:
+            raise exceptions.DoesNotExistException
+
+    def _delete_alarm_inhibition_definition_source_match(self, conn, _id):
+        conn.execute(self.delete_aidsm_query,
+                     b_alarm_inhibition_definition_id=_id)
+
+    def _delete_alarm_inhibition_definition_target_match(self, conn, _id):
+        conn.execute(self.delete_aidtm_query,
+                     b_alarm_inhibition_definition_id=_id)
