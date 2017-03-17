2843a8ede9002c40ccab70fba3f20ae5dc2c67a2
Kaiyan Sheng <kaiyan.sheng@hp.com>
2017-03-17 10:40:07 -0600

    Add alarm rule table in mysql for querying
    With alarm grouping, inhibition and silencing, we need to store the
    rule id associated with alarm definition id into mysql for further
    queries.
    
    Partially-Implements: blueprint alarm-inhibition
    Partially-Implements: blueprint alarm-silencing
    Partially-Implements: blueprint alarm-grouping
    Story: 2000935
    Task: 4063
    
    Change-Id: I126e9a8e9fdec9f1f1a7f0c09033c256f4099b54

--- devstack/files/schema/mon_mysql.sql
+++ devstack/files/schema/mon_mysql.sql
@@ -1,5 +1,5 @@
 /*
-* (C) Copyright 2015,2016 Hewlett Packard Enterprise Development LP
+* (C) Copyright 2015-2017 Hewlett Packard Enterprise Development LP
 * Copyright 2017 FUJITSU LIMITED
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -87,6 +87,30 @@ CREATE TABLE `alarm_metric` (
   KEY `alarm_id` (`alarm_id`),
   KEY `metric_definition_dimensions_id` (`metric_definition_dimensions_id`),
   CONSTRAINT `fk_alarm_id` FOREIGN KEY (`alarm_id`) REFERENCES `alarm` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `alarm_rule_definition` (
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tenant_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tenant_id` (`tenant_id`),
+  KEY `deleted_at` (`deleted_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `alarm_rule` (
+  `alarm_rule_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alarm_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `inhibition_source_id` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL, 
+  KEY `fk_rules_alarm_id` (`alarm_id`),
+  PRIMARY KEY (`alarm_rule_id`,`alarm_id`),
+  CONSTRAINT `fk_rules_alarm_id` FOREIGN KEY (`alarm_id`) REFERENCES `alarm` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_alarm_rule_definition_id` FOREIGN KEY (`alarm_rule_id`) REFERENCES `alarm_rule_definition` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_inhibition_source_id` (`inhibition_source_id`) REFERENCES `alarm` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `metric_definition` (
