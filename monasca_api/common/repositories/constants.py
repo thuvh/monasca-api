d04f7b5bd3598ce7cf6a93a9a7247d7cfc798365
Kaiyan Sheng <kaiyan.sheng@hpe.com>
2017-05-18 16:24:22 -0600

    Add API Resources for Group, Inhibit and Silence Rules
        Group rule is used to categorize alarms of similar nature into
        a single notification. This functionality is very usefully
        especially during large outages when many systems fail at once and
        thousands of alarms go off simultaneously.
    
        Inhibit rule allows user to mute a set of alarms (target alarms)
        given that another alarm (source alarm) is firing. For example,
        we can use this to mute any low level alarms if the same alarm is
        already critical.
    
        Silence rule will mute alarms for a given time based on
        input parameters: start_time and end_time. A silence is configured
        based on matchers and if an incoming alarm is checked to match the
        matchers, no notifications will be sent out for that alarm.
    
        This review is to merge several reviews together so that testing will
        be easier:
        https://review.openstack.org/#/c/432499/
        https://review.openstack.org/#/c/435632/
        https://review.openstack.org/#/c/436698/
        https://review.openstack.org/#/c/447060/
    
        Story: 2000935
        Task: 4068
        Tast: 4067
        Task: 4059
    
    Depends-On: I3c12c3737ddac064ee2665b6e79369c7613cbea9
    Depends-On: I85b8bfc9338ee17709ec30aeb4b807b3bc12404e
    
    Change-Id: I3509be99311e98031ffb985b8cd6f1c1562ecbd5
    Co-Authored-By: Andrea Adams <aadams@hpe.com>

--- monasca_api/common/repositories/constants.py
+++ monasca_api/common/repositories/constants.py
@@ -1,1 +1,31 @@
+# (C) Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 PAGE_LIMIT = 10000
+# Brief description of the group rule
+GROUP_RULE_DESCRIPTION = ''
+# Brief description of the silence rule
+SILENCE_RULE_DESCRIPTION = ''
+# Brief description of the inhibit rule
+INHIBIT_RULE_DESCRIPTION = ''
+# The length of time the silence should effect alarm state transitions.
+# Defaults to '10m', also can be specified in "2d3h4m5s" format.
+SILENCE_RULE_SILENCE_DURATION = '10m'
+# Group wait specifies the window that the Notification Engine waits for
+# notifications to pull down a batch to be examined
+GROUP_RULE_GROUP_WAIT = '30s'
+# Repeat interval specifies the wait time to resend the group notification.
+GROUP_RULE_REPEAT_INTERVAL = '2h'
+# Array of notification method IDs that are invoked when the group of alarms
+# transition to any state.
+GROUP_RULE_ACTIONS = []
