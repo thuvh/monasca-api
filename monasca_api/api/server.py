5a3019977703b99888626fdc07a20ee086facf8b
Andrea Adams <aadams@hpe.com>
2017-05-18 16:24:22 -0600

    Add API Resources for Alarm Group, Inhibition and Silence
        Alarm grouping is used to categorize alarms of similar nature into
        a single notification. This functionality is very usefully
        especially during large outages when many systems fail at once and
        thousands of alarms go off simultaneously.
    
        Inhibition allows user to mute a set of alarms (target alarms)
        given that another alarm (source alarm) is firing. For example,
        we can use this to mute any low level alarms if the same alarm is
        already critical.
    
        Silencing manager will mute alarms for a given time based on
        input parameters: start_time and end_time. A silence is configured
        based on matchers and if an incoming alarm is checked to match the
        matchers, no notifications will be sent out for that alarm.
    
        This review is to merge several reviews together so that testing will
        be easier:
        https://review.openstack.org/#/c/432499/
        https://review.openstack.org/#/c/435632/
        https://review.openstack.org/#/c/436698/
        https://review.openstack.org/#/c/447060/
    
        Story: 2000935
        Task: 4068
        Tast: 4067
        Task: 4059
    
    Change-Id: Id19180664ac17fb9e641c3cfbc965294f0534e0a
    Co-Authored-By: Andrea Adams <aadams@hpe.com>

--- monasca_api/api/server.py
+++ monasca_api/api/server.py
@@ -1,5 +1,5 @@
 # Copyright 2014 IBM Corp
-# (C) Copyright 2015,2016 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2015-2017 Hewlett Packard Enterprise Development LP
 # Copyright 2017 Fujitsu LIMITED
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -51,7 +51,13 @@ dispatcher_opts = [cfg.StrOpt('versions', default=None,
                    cfg.StrOpt('dimension_names', default=None,
                               help='Dimension names'),
                    cfg.StrOpt('notification_method_types', default=None,
-                              help='notification_method_types methods')]
+                              help='notification_method_types methods'),
+                   cfg.StrOpt('alarm_group_definitions', default=None,
+                              help='Alarm group definitions'),
+                   cfg.StrOpt('alarm_inhibition_definitions', default=None,
+                              help='Alarm inhibition definitions'),
+                   cfg.StrOpt('alarm_silence_definitions', default=None,
+                              help='Alarm silence definitions')]
 
 dispatcher_group = cfg.OptGroup(name='dispatcher', title='dispatcher')
 cfg.CONF.register_group(dispatcher_group)
@@ -128,6 +134,24 @@ def launch(conf):
     notification_method_types = simport.load(
         cfg.CONF.dispatcher.notification_method_types)()
     app.add_route("/v2.0/notification-methods/types", notification_method_types)
+
+    alarm_group_definitions = simport.load(
+        cfg.CONF.dispatcher.alarm_group_definitions)()
+    app.add_route("/v2.0/alarm-group-definitions/", alarm_group_definitions)
+    app.add_route("/v2.0/alarm-group-definitions/{alarm_group_definition_id}",
+                  alarm_group_definitions)
+
+    alarm_inhibition_definitions = simport.load(
+        cfg.CONF.dispatcher.alarm_inhibition_definitions)()
+    app.add_route("/v2.0/alarm-inhibition-definitions/", alarm_inhibition_definitions)
+    app.add_route("/v2.0/alarm-inhibition-definitions/{alarm_inhibition_definition_id}",
+                  alarm_inhibition_definitions)
+    alarm_silence_definitions = simport.load(
+        cfg.CONF.dispatcher.alarm_silence_definitions)()
+    app.add_route("/v2.0/alarm-silence-definitions/",
+                  alarm_silence_definitions)
+    app.add_route("/v2.0/alarm-silence-definitions/{"
+                  "alarm_silence_definition_id}", alarm_silence_definitions)
 
     LOG.debug('Dispatcher drivers have been added to the routes!')
     return app
