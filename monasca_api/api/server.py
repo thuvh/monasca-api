4d341993e745c4d84a750d61af6d62a6e423f521
Kaiyan Sheng <kaiyan.sheng@hpe.com>
2017-05-18 16:24:22 -0600

    Add API Resources for Group, Inhibit and Silence Rules
        Group rule is used to categorize alarms of similar nature into
        a single notification. This functionality is very usefully
        especially during large outages when many systems fail at once and
        thousands of alarms go off simultaneously.
    
        Inhibit rule allows user to mute a set of alarms (target alarms)
        given that another alarm (source alarm) is firing. For example,
        we can use this to mute any low level alarms if the same alarm is
        already critical.
    
        Silence rule will mute alarms for a given time based on
        input parameters: start_time and end_time. A silence is configured
        based on matchers and if an incoming alarm is checked to match the
        matchers, no notifications will be sent out for that alarm.
    
        This review is to merge several reviews together so that testing will
        be easier:
        https://review.openstack.org/#/c/432499/
        https://review.openstack.org/#/c/435632/
        https://review.openstack.org/#/c/436698/
        https://review.openstack.org/#/c/447060/
    
        Story: 2000935
        Task: 4068
        Tast: 4067
        Task: 4059
    
    Change-Id: I3509be99311e98031ffb985b8cd6f1c1562ecbd5
    Co-Authored-By: Andrea Adams <aadams@hpe.com>

--- monasca_api/api/server.py
+++ monasca_api/api/server.py
@@ -1,5 +1,5 @@
 # Copyright 2014 IBM Corp
-# (C) Copyright 2015,2016 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2015-2017 Hewlett Packard Enterprise Development LP
 # Copyright 2017 Fujitsu LIMITED
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -53,8 +53,13 @@ dispatcher_opts = [cfg.StrOpt('versions', default=None,
                    cfg.StrOpt('notification_method_types', default=None,
                               help='notification_method_types methods'),
                    cfg.StrOpt('healthchecks', default=None,
-                              help='Health checks endpoint')]
-
+                              help='Health checks endpoint'),
+                   cfg.StrOpt('group_rules', default=None,
+                              help='Group rules'),
+                   cfg.StrOpt('inhibit_rules', default=None,
+                              help='Inhibit rules'),
+                   cfg.StrOpt('silence_rules', default=None,
+                              help='Silence rules')]
 dispatcher_group = cfg.OptGroup(name='dispatcher', title='dispatcher')
 cfg.CONF.register_group(dispatcher_group)
 cfg.CONF.register_opts(dispatcher_opts, dispatcher_group)
@@ -141,6 +146,18 @@ def launch(conf):
 
     healthchecks = simport.load(cfg.CONF.dispatcher.healthchecks)()
     app.add_route("/healthcheck", healthchecks)
+
+    group_rules = simport.load(cfg.CONF.dispatcher.group_rules)()
+    app.add_route("/v2.0/group-rules/", group_rules)
+    app.add_route("/v2.0/group-rules/{group_rule_id}", group_rules)
+
+    inhibit_rules = simport.load(cfg.CONF.dispatcher.inhibit_rules)()
+    app.add_route("/v2.0/inhibit-rules/", inhibit_rules)
+    app.add_route("/v2.0/inhibit-rules/{inhibit_rule_id}", inhibit_rules)
+
+    silence_rules = simport.load(cfg.CONF.dispatcher.silence_rules)()
+    app.add_route("/v2.0/silence-rules/", silence_rules)
+    app.add_route("/v2.0/silence-rules/{silence_rule_id}", silence_rules)
 
     LOG.debug('Dispatcher drivers have been added to the routes!')
     return app
