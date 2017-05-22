dc861c6e00a89f1091589b435ca308e5af3c19d9
Kaiyan Sheng <kaiyan.sheng@hp.com>
2017-05-22 13:30:13 -0600

    Change to "monasca-read-only-user" for read only tests
    "monasca-read-only-user" is a better name instead of "anotherrole"
    for read only tempest tests. It's also set by default in
    api-config.conf
    Also change test.attr() to decorators.attr() in tempest tests
    because of the deprecation warning.
    
    Change-Id: I05427147e2755bdc6fde483a145258f6fb695356

--- monasca_tempest_tests/tests/api/test_metrics_names.py
+++ monasca_tempest_tests/tests/api/test_metrics_names.py
@@ -18,7 +18,7 @@ from monasca_tempest_tests.tests.api import base
 from monasca_tempest_tests.tests.api import constants
 from monasca_tempest_tests.tests.api import helpers
 from tempest.lib.common.utils import data_utils
-from tempest import test
+from tempest.lib import decorators
 from urllib import urlencode
 
 
@@ -70,13 +70,13 @@ class TestMetricsNames(base.BaseMonascaTest):
     def resource_cleanup(cls):
         super(TestMetricsNames, cls).resource_cleanup()
 
-    @test.attr(type='gate')
+    @decorators.attr(type='gate')
     def test_list_metrics_names(self):
         resp, response_body = self.monasca_client.list_metrics_names()
         metric_names = self._verify_response(resp, response_body)
         self.assertEqual(metric_names, self._expected_names_list)
 
-    @test.attr(type='gate')
+    @decorators.attr(type='gate')
     def test_list_metrics_names_with_dimensions(self):
         query_params = '?dimensions=' + self._dimensions_param
         resp, response_body = self.monasca_client.list_metrics_names(
@@ -85,7 +85,7 @@ class TestMetricsNames(base.BaseMonascaTest):
         self.assertEqual(metric_names,
                          self._test_metric_names_with_same_dim)
 
-    @test.attr(type='gate')
+    @decorators.attr(type='gate')
     def test_list_metrics_names_with_limit_offset(self):
         resp, response_body = self.monasca_client.list_metrics_names()
         self.assertEqual(200, resp.status)
@@ -125,7 +125,7 @@ class TestMetricsNames(base.BaseMonascaTest):
                 # Get the next set
                 offset = self._get_offset(response_body)
 
-    @test.attr(type='gate')
+    @decorators.attr(type='gate')
     def test_list_metrics_names_with_offset_not_in_metrics_names_list(self):
         offset1 = 'tempest-abc'
         offset2 = 'tempest-name111'
