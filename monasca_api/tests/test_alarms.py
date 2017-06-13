f0be765c0687fcb7ec921d03d6bd945d05d598f7
Habeeb Mohammed <habeeb.mohammed@hpe.com>
2017-06-13 11:24:19 -0600

    Fixed endpoint required parameters for DELETE
    	Problem: Received HTTP 500 Internal Server Error when using DELETE in query
    	without specifying alarm definition id.
    
    	Solution: Modified on_delete() so that if alarm definition id
    	is not specified, query response will be HTTP 422 Unprocessible Entity.
    
    Change-Id: Ie4518cb5a082211190f0364673944ecfca43948e

--- monasca_api/tests/test_alarms.py
+++ monasca_api/tests/test_alarms.py
@@ -434,6 +434,16 @@ class TestAlarmDefinition(AlarmTestBase):
 
         self.assertEqual(self.srmock.status, falcon.HTTP_422)
 
+    def test_alarm_definition_delete_no_id(self):
+        alarm_def = {
+            u'name': u'Test Alarm Definition Updated',
+        }
+
+        self.simulate_request("/v2.0/alarm-definitions/", headers={'X-Roles': 'admin', 'X-Tenant-Id': TENANT_ID},
+                              method="DELETE", body=json.dumps(alarm_def))
+
+        self.assertEqual(self.srmock.status, falcon.HTTP_422)
+
     def test_alarm_definition_patch(self):
         self.alarm_def_repo_mock.return_value.get_alarm_definitions.return_value = []
         description = u'Non-ASCII character: \u2603'
