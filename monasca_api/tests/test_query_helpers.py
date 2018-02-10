e05d93fa493ee2b3d75f3aff417cb64af578c960
Monasca CI <aadams@hpe.com>
2018-02-09 17:03:38 -0700

    Reject metrics furure and past metrics
    The Python Monasca API should reject metrics outside of some
    reasonable time window. Wrong timestamps are probably
    symptomatic of an NTP problem and should not make it to the
    Threshold engine because that leads to inaccurate alarm and
    notification evaluations.
    
    Story: 2001535
    
    Change-Id: I69c2bd547a5d2f63c62ffdc9b07df6be6e599c6c

--- monasca_api/tests/test_query_helpers.py
+++ monasca_api/tests/test_query_helpers.py
@@ -14,10 +14,16 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import time
+
 from mock import Mock
 
 from monasca_api.tests import base
+from monasca_api.v2.common.exceptions import HTTPUnprocessableEntityError
 from monasca_api.v2.reference import helpers
+
+ONE_MINUTE = 60 * 1000
+ONE_WEEK = 7 * 24 * 60 * 60 * 1000
 
 
 class TestGetQueryDimension(base.BaseTestCase):
@@ -141,3 +147,53 @@ class TestGetOldQueryParams(base.BaseTestCase):
 
         helpers._get_old_query_params_except_offset(result, uri)
         self.assertEqual(result, ["foo=spam%3Dham"])
+
+
+class TestValidateTimestampRange(base.BaseTestCase):
+
+    def test_validate_timestamp_range(self):
+        timestamp = time.time() * 1000
+        metric = {'timestamp': timestamp}
+        try:
+            helpers.validate_timestamp_range(metric, should_validate=True,
+                                             future_seconds=ONE_MINUTE,
+                                             past_seconds=ONE_WEEK)
+        except HTTPUnprocessableEntityError:
+            self.fail("Test validate timestamp failed.")
+
+    def test_validate_timestamp_range_invalid_future(self):
+        timestamp = time.time() * 1000 + ONE_MINUTE * 2
+        metric = {'timestamp': timestamp}
+        try:
+            helpers.validate_timestamp_range(metric, should_validate=True,
+                                             future_seconds=ONE_MINUTE,
+                                             past_seconds=ONE_WEEK)
+        except HTTPUnprocessableEntityError:
+            pass
+        else:
+            self.fail("Test validate timestamp should have failed with"
+                      " HTTPUnprocessableEntityError.")
+
+    def test_validate_timestamp_range_invalid_past(self):
+        timestamp = time.time() * 1000 - ONE_WEEK
+        metric = {'timestamp': timestamp}
+        try:
+            helpers.validate_timestamp_range(metric, should_validate=True,
+                                             future_seconds=ONE_MINUTE,
+                                             past_seconds=ONE_WEEK)
+        except HTTPUnprocessableEntityError:
+            pass
+        else:
+            self.fail("Test validate timestamp should have failed with"
+                      " HTTPUnprocessableEntityError.")
+
+    def test_validate_timestamp_range_turn_off(self):
+        timestamp = time.time() * 1000 + ONE_MINUTE * 2  # Invalid future time
+        metric = {'timestamp': timestamp}
+        try:
+            helpers.validate_timestamp_range(metric, should_validate=False,
+                                             future_seconds=ONE_MINUTE,
+                                             past_seconds=ONE_WEEK)
+        except HTTPUnprocessableEntityError:
+            self.fail("Test validate timestamp failed. "
+                      "Configured flag should skip validation")
