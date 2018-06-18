2b5d5064f2b1fa4e41e3859e11bf82c1e1afd3b2
Adrian Czarnecki <adrian.czarnecki@ts.fujitsu.com>
2018-06-18 12:59:54 +0200

    [WIP] enable python3 in devstack
    Change-Id: I44f09c9068ecd682d7c91945a5e32842ab335b80

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -768,7 +768,8 @@ function install_monasca-api {
     echo_summary "Install Monasca monasca_api "
 
     git_clone $MONASCA_API_REPO $MONASCA_API_DIR $MONASCA_API_BRANCH
-    setup_develop $MONASCA_API_DIR
+    _setup_package_with_constraints_edit $MONASCA_API_DIR
+    #setup_develop $MONASCA_API_DIR
 
     install_monasca_common
 
@@ -1130,7 +1131,11 @@ function install_monasca_agent {
     sudo mkdir -p /opt/monasca-agent || true
     sudo chown $STACK_USER:monasca /opt/monasca-agent
 
-    (cd /opt/monasca-agent ; virtualenv .)
+    if python3_enabled; then
+        (cd /opt/monasca-agent ; virtualenv -p python3 .)
+    else
+        (cd /opt/monasca-agent ; virtualenv .)
+    fi
 
     PIP_VIRTUAL_ENV=/opt/monasca-agent
 
