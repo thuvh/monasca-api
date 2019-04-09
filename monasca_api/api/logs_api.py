3fa2b76296d93d29634725f4c2a25867be25fd39
Adrian Czarnecki <adrian.czarnecki@ts.fujitsu.com>
2019-04-09 16:10:58 +0200

    [WIP] Merge log-api and api
    Change-Id: Iaa5689694e7081f3375f1a2235cad31d6a7b5f76

--- monasca_api/api/logs_api.py
+++ monasca_api/api/logs_api.py
@@ -14,13 +14,9 @@
 # under the License.
 
 import falcon
-from monasca_log_api import conf
-from monasca_log_api.monitoring import client
-from monasca_log_api.monitoring import metrics
 from oslo_log import log
 
 
-CONF = conf.CONF
 LOG = log.getLogger(__name__)
 
 
@@ -34,30 +30,7 @@ class LogsApi(object):
     """
     def __init__(self):
         super(LogsApi, self).__init__()
-        if CONF.monitoring.enable:
-            self._statsd = client.get_client()
-
-            # create_common counters, gauges etc.
-            self._metrics_dimensions = dimensions = {'version': self.version}
-
-            self._logs_in_counter = self._statsd.get_counter(
-                name=metrics.LOGS_RECEIVED_METRIC,
-                dimensions=dimensions
-            )
-            self._logs_size_gauge = self._statsd.get_gauge(
-                name=metrics.LOGS_RECEIVED_BYTE_SIZE_METRICS,
-                dimensions=dimensions
-            )
-            self._logs_rejected_counter = self._statsd.get_counter(
-                name=metrics.LOGS_REJECTED_METRIC,
-                dimensions=dimensions
-            )
-            self._logs_processing_time = self._statsd.get_timer(
-                name=metrics.LOGS_PROCESSING_TIME_METRIC,
-                dimensions=dimensions
-            )
-
-        LOG.info('Initializing LogsApi %s!' % self.version)
+        LOG.info('Initializing LogsApi')
 
     def on_post(self, req, res):
         """Accepts sent logs as text or json.
@@ -83,6 +56,3 @@ class LogsApi(object):
         """
         res.status = falcon.HTTP_501  # pragma: no cover
 
-    @property
-    def version(self):
-        return getattr(self, 'VERSION')
