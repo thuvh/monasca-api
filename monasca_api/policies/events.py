f1e93e61ba164619f3844734767abde5480b2342
Adrian Czarnecki <adrian.czarnecki@ts.fujitsu.com>
2020-01-16 15:32:35 +0100

    [WIP] Merge events-api
    Change-Id: I01ac39ae19bf4fe70a696bf88a7ddf42bb85d6a1

---
+++ monasca_api/policies/events.py
@@ -0,0 +1,42 @@
+# Copyright 2019 FUJITSU LIMITED
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from monasca_api import policies
+from oslo_config import cfg
+from oslo_policy import policy
+
+
+DEFAULT_AUTHORIZED_ROLES = policies.roles_list_to_check_str(
+    cfg.CONF.security.default_authorized_roles)
+AGENT_AUTHORIZED_ROLES = policies.roles_list_to_check_str(
+    cfg.CONF.security.agent_authorized_roles)
+DELEGATE_AUTHORIZED_ROLES = policies.roles_list_to_check_str(
+    cfg.CONF.security.delegate_authorized_roles)
+
+rules = [
+    policy.DocumentedRuleDefault(
+        name='api:events:post',
+        check_str=' or '.join(filter(None, [AGENT_AUTHORIZED_ROLES,
+                                            DEFAULT_AUTHORIZED_ROLES,
+                                            DELEGATE_AUTHORIZED_ROLES])),
+        description='Events post rule',
+        operations=[
+            {'path': '/events', 'method': 'POST'},
+        ]
+    )
+]
+
+
+def list_rules():
+    return rules
